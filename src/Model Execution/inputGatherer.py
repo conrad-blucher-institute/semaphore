3bd8cdf2eb7cccde2f2f029631d3b8038e58e899
Kastl <mkastl@islander.tamucc.edu>
2023-02-12 16:53:50 -0600

    Built the DSPEC parser, created methods to load inputs from a csv, refactored code to be easier to read and not so bloated.

--- src/Model Execution/inputGatherer.py
+++ src/Model Execution/inputGatherer.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#utility.py
+#inputGatherer.py
 #----------------------------------
 # Created By : Matthew Kastl
 # Created Date: 2/3/2023
@@ -12,8 +12,53 @@ the dspec file and pulling inputs from different places.
 # 
 #
 #Input
+from os import path
+from utility import log
+from json import load
+from csv import reader
+import datetime
+
 class InputGatherer:
-    def __init__(self, dspecFilePath: str) -> None:
-        pass
-    def pullInputs(self) -> list[int]:
-        pass
+    def __init__(self, dspecFilePath: path) -> None:
+        """Constructor sends the specFile off to be loaded and parsed
+        """
+        self.__parse_dspec(dspecFilePath)
+    
+    def __parse_dspec(self, dspecFilePath: path) -> None:
+        """Loads a dspec as a JSON file and parses out the options and input sepcifications. 
+        It saves them as private class objects.
+        """
+        if not path.exists(dspecFilePath):
+            log(f'dspecFilePath not found!')
+            raise FileNotFoundError
+        
+        with open(dspecFilePath) as dspecFile:
+            self.__dspecDict = load(dspecFile)
+            self.__options = self.__dspecDict['options']
+            self.__inputSpecification = self.__dspecDict['inputs']
+    
+    def __pull_inputs_from_CSV(self) -> list[any]:
+        """Pulls inputs from a CSV path. Assumes one row and no headers. Returns every column.
+        """
+        if not path.exists(self.__options['importPath']): 
+            log(f'CVS path not valid for {self.__dspecDict["Name"]}!')
+            raise FileNotFoundError
+
+        with open(self.__options['importPath'], newline= '') as csvfile:
+            return list(reader(csvfile))[0]
+    
+    def get_model_name(self) -> str:
+        """Returns the name of the model as specified in the DSPEC file."""
+        return self.__dspecDict['modelName']       
+
+    def get_inputs(self, dateTime: datetime) -> list[any]:
+        """Public method that reads the import method from the dspec file and starts execution to
+        gather said inputs. Returns the inputs as an array.
+        """
+        #TODO::Handle for requested date
+        match self.__options['importMethod']:
+            case 'fromLocalSCV': return self.__pull_inputs_from_CSV()
+            case _:
+                log(f'Failed to catch importMethod from dspec {self.__dspecDict["Name"]}!')
+                raise SyntaxError
+
--- src/Model Execution/modelWrapper.py
+++ src/Model Execution/modelWrapper.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
-#utility.py
+#modelWrapper.py
 #----------------------------------
 # Created By : Matthew Kastl
 # Created Date: 2/3/2023
 # version 1.0
 #----------------------------------
-""" This file houses the ModelWrapper class. The class functions to run an
-AI model from an AI file and call an inputGatherer to parse a dspecFilePath
-and pull the inputs.
+""" This script houses the ModelWrapper class. The class functions to run an
+AI model from an H5 file from a datetime and inputs specified in a dspec file.
  """ 
 #----------------------------------
 # 
@@ -17,41 +16,44 @@ from os import path
 from utility import log
 from tensorflow.keras.models import load_model
 from inputGatherer import InputGatherer
+import datetime
 
 
 class ModelWrapper:
-    def __init__(self, h5FilePath: str, dspecFilePath: str) -> None:
-        if not path.exists(h5FilePath):
-            log("h5FilePath does not result in an existing file!")
+    def __init__(self, dspecFilePath: str) -> None:
+        """Constructor generates an InputGatherer parse the dspec file 
+        and attempts to load the model.
+        """
+        self.__inputGatherer = InputGatherer(self._dspecFilePath)
+        self.__load_modele()
+
+
+    def __load_modele(self) -> None:
+        """Private method to load a model saves as an h5 file designated
+        in the dspec file using Tenserflow/Karas
+        """
+        modelName = self.__inputGatherer.get_model_name()
+        h5FilePath = modelName if modelName.endswith('.h5') else modelName + '.h5'
+
+        if not path.exists(h5FilePath): 
+            log(f'H5 file not found!')
             raise FileNotFoundError
-        if not path.exists(dspecFilePath):
-            log("dspecFilePath does not result in an existing file!")
-            raise FileNotFoundError
 
-        self._h5filePath = h5FilePath
-        self.dspecFilePath = dspecFilePath
-
-
-    def loadModele(self) -> None:
         try:
-            self._model = load_model(self.h5FilePath)
-            self._model.summary()
-            self._model.get_weights()
+            self._model = load_model(h5FilePath)
+            self._model.summary() #TODO::check to remove
+            self._model.get_weights() #TODO::check to remove
+        except Exception as e:
+            log(e) 
+            raise e
+
+
+    def make_prediction(self, dateTime: datetime) -> any:
+        """Public method to generate a prediction given a datetime.
+        """
+        try:
+            self.__inputs = self.inputGatherer.get_inputs(dateTime)
+            return self._model.predict(self.__inputs) 
         except Exception as e:
             log(e)
-
-
-    def loadInputs(self) -> None:
-        try:
-            inputGatherer = InputGatherer(self._dspecFilePath)
-            self._inputs = inputGatherer.pullInputs
-        except Exception as e:
-            log(e)
-
-    def makePrediction(self) -> any:
-        try:
-            self.loadModele()
-            self.loadInputs()
-            return self._model.predict(self._inputs) 
-        except Exception as e:
-            log(e)
+            raise e
