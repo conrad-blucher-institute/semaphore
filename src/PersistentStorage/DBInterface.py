306486351a268135b400ab6542e67d5b1fc32641
Kastl <mkastl@islander.tamucc.edu>
2023-03-26 16:47:51 -0500

    Checking db matches dbml file, and adding in foriegn keys

--- src/PersistentStorage/DBInterface.py
+++ src/PersistentStorage/DBInterface.py
@@ -1,6 +1,6 @@
       
 from sqlalchemy import create_engine
-from sqlalchemy import Table, Column, Integer, String, DateTime, Float, MetaData, UniqueConstraint, Engine
+from sqlalchemy import Table, Column, Integer, String, DateTime, Float, MetaData, UniqueConstraint, Engine, ForeignKey
 
 class DBInterface():
     
@@ -36,22 +36,23 @@ class DBInterface():
 
         self._metadata_obj = MetaData()
         
+        #this table stores the actual data values as retrieved or received 
         self.s_data_point = Table(
             "s_data_point",
             self._metadata_obj,
 
             Column("id", Integer, autoincrement=True, primary_key=True),
 
-            Column("timeActualized", DateTime, nullable=False),
-            Column("timeAquired", DateTime, nullable=False),
+            Column("timeActualized", DateTime, nullable=False), #timestamp for the value in UTC GMT
+            Column("timeAquired", DateTime, nullable=False),    #When the data was inserted by us
 
-            Column("dataValue", String(20), nullable=False),
-            Column("unitsCode", String(10), nullable=False),
+            Column("dataValue", String(20), nullable=False),    #Actual value for data points
+            Column("unitsCode", String(10), ForeignKey("s_ref_units.code"), nullable=False), #unit the value is stored in
 
-            Column("dataSourceCode", String(10), nullable=False),
-            Column("sLocationCode", String(25), nullable=False),
-            Column("seriesCode", String(10), nullable=False),
-            Column("datumCode", String(10), nullable=True),
+            Column("dataSourceCode", String(10), ForeignKey("s_ref_data_source.code"), nullable=False), #CBI specific ID for the Location
+            Column("sLocationCode", String(25), ForeignKey("s_ref_slocation.code"), nullable=False),    #the code for the source from which the value was obtained e.g, NOAA
+            Column("seriesCode", String(10), ForeignKey("s_ref_series.code"), nullable=False),          #The code fot the type of measurment or prediction e.g, wdir
+            Column("datumCode", String(10), ForeignKey("s_ref_datum.code"), nullable=True),             #the datum(e.g., water-level, harmonic)
             Column("latitude", String(16), nullable=True),
             Column("longitude", String(16), nullable=True),
 
@@ -64,38 +65,46 @@ class DBInterface():
 
             Column("id", Integer, autoincrement=True, primary_key=True),
 
-            Column("timeGenerated", DateTime, nullable=False),
-            Column("leadTime", Float, nullable=False),
+            Column("timeGenerated", DateTime, nullable=False),  #The time at which the prediction was made
+            Column("leadTime", Float, nullable=False),          #the amount of hours till the predicted even occurs
 
-            Column("dataValue", String(20), nullable=False),
-            Column("unitsCode", String(10), nullable=False),
+            Column("dataValue", String(20), nullable=False),    #the actual value
+            Column("unitsCode", String(10), ForeignKey("s_ref_units.code"), nullable=False), #the units the data point is stored in
 
-            Column("resultCode", String(10), nullable=False),
+            Column("resultCode", String(10), nullable=True), #some value that discribes the quality of the pridiction
+            Column("resultCodeUnit", String(10), ForeignKey("s_ref_resultCodeUnits.code"), nullable=True), #how that quality is stored
 
-            Column("dataSourceCode", String(10), nullable=False),
-            Column("sLocationCode", String(25), nullable=False),
-            Column("seriesCode", String(10), nullable=False),
-            Column("datumCode", String(10), nullable=False),
+            Column("dataSourceCode", String(10), ForeignKey("s_ref_data_source.code"), nullable=False),     #CBI specific ID for the Location
+            Column("sLocationCode", String(25), ForeignKey("s_ref_slocation.code"), nullable=False),        #the code for the source from which the value was obtained e.g, NOAA
+            Column("seriesCode", String(10), ForeignKey("s_ref_series.code"), nullable=False),              #The code fot the type of measurment or prediction e.g, wdir
+            Column("datumCode", String(10), ForeignKey("s_ref_datum.code"), nullable=True),                 #the datum(e.g., water-level, harmonic)
             Column("latitude", String(16), nullable=False),
             Column("longitude", String(16), nullable=False),
 
-            UniqueConstraint("timeGenerated", "leadTime", "dataValue", "unitsCode", "resultCode", "dataSourceCode", "sLocationCode", "seriesCode"),
+            UniqueConstraint("timeGenerated", "leadTime", "dataValue", "unitsCode", "dataSourceCode", "sLocationCode", "seriesCode"),
         )
 
+        #This table maps CBI location codes to location codes used by datasorces
         self.s_locationCode_dataSourceLocationCode_mapping = Table(
             "s_locationCode_dataSourceLocationCode_mapping",
             self._metadata_obj,
 
             Column("id", Integer, autoincrement=True, primary_key=True),
             
-            Column("sLocationCode", String(25), nullable=False),
-            Column("dataSourceCode", String(10), nullable=False),
-            Column("dataSourceLocationCode", String(255), nullable=False),
-            Column("priorityOrder", Integer, nullable=False),
+            Column("sLocationCode", String(25), ForeignKey("s_ref_slocation.code"), nullable=False),    #Local term
+            Column("dataSourceCode", String(10), ForeignKey("s_ref_data_source.code"), nullable=False), #Data source
+            Column("dataSourceLocationCode", String(255), nullable=False),                              #Forien term
+            Column("priorityOrder", Integer, nullable=False),                                           #priority of use
 
             UniqueConstraint("sLocationCode", "dataSourceCode", "dataSourceLocationCode", "priorityOrder"),
         )
 
+
+        #The rest of these tables are reference tables for values stored in the tables above. They all contain
+        # ID - aoutincamented id
+        # code - that mapped code
+        # display name - a non compressed pretty name
+        # notes - more information about that item
         self.s_ref_slocation = Table(
             "s_ref_slocation",
             self._metadata_obj,
@@ -156,8 +165,8 @@ class DBInterface():
             UniqueConstraint("code", "displayName"),
         )
         
-        self.s_ref_resultCode = Table(
-            "s_ref_resultCode",
+        self.s_ref_resultCodeUnits = Table(
+            "s_ref_resultCodeUnits",
             self._metadata_obj,
 
             Column("id", Integer, autoincrement=True, primary_key=True),
