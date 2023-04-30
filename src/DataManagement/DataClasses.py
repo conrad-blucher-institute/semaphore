3c0ad6d5bec8508da49a3a137f49b7832557ef70
Kastl <mkastl@islander.tamucc.edu>
2023-04-30 18:16:09 -0500

    Data Manager v1

--- src/DataManagement/DataClasses.py
+++ src/DataManagement/DataClasses.py
@@ -1,5 +1,20 @@
+# -*- coding: utf-8 -*-
+#DataIngestionMap.py
+#----------------------------------
+# Created By: Matthew Kastl
+# Created Date: 4/30/2023
+# version 1.0
+#----------------------------------
+"""This file holds for classes that each act as a structer for the semaphore program. A request is a single request for something like a single input specification. 
+A response is the obv response to the request. A Data point is a static point of data with a single datetime of when it was actualized.
+A Prediction is a datapoint that was predicted by an AI. Includes the datetime of when it was generated and the lead time of the span from when the prediction was made
+to when it could be verified.
+ """ 
+#----------------------------------
+# 
+#
+#Inports
 from datetime import datetime
-
 
 class DataPoint():
     def __init__(self, value: str, unit: str, dateTime: datetime) -> None:
@@ -24,13 +39,12 @@ class Prediction():
 
 
 class Request():
-    def __init__(self, source: str, series: str, location: str, unit: str, isPrediction, fromDateTime: datetime, toDateTime: datetime, datum: str = None) -> None:
+    def __init__(self, source: str, series: str, location: str, unit: str, fromDateTime: datetime, toDateTime: datetime, datum: str = None) -> None:
         self.source = source
         self.series = series
         self.location = location
         self.unit = unit
         self.datum = datum
-        self.isPrediction = isPrediction
         self.fromDateTime = fromDateTime
         self.toDateTime = toDateTime
 
@@ -39,13 +53,12 @@ class Request():
 
 
 class Response():
-    def __init__(self, source: str, series: str, location: str, unit: str, datum: str = None) -> None:
-        self.source = source
-        self.series = series
-        self.location = location
-        self.unit = unit
-        self.datum = datum
+    def __init__(self, request: Request, wasSuccessful: bool, errorReason: str = None) -> None:
+        self.request = request
         self.data = []
+        self.wasSuccessful = wasSuccessful
+        self.errorReason = errorReason
 
     def __str__(self) -> str:
-        return f'[Response] -> source: {self.source}, series: {self.series}, location: {self.location}, unit: {self.unit}, datum {self.datum}, data {self.data}'
+        return f'[Response] -> request: {self.request}, data: {self.data}, wasSuccessful: {self.wasSuccessful}, errorReason: {self.errorReason}'
+    
--- src/DataManagement/DataManager.py
+++ src/DataManagement/DataManager.py
@@ -1,12 +1,200 @@
+# -*- coding: utf-8 -*-
+#DataIngestionMap.py
+#----------------------------------
+# Created By: Matthew Kastl
+# Created Date: 4/30/2023
+# version 1.0
+#----------------------------------
+"""This class holds is the startpoint for interacting with the data section of semaphore. All data requests should go through here.
+ """ 
+#----------------------------------
+# 
+#
+#Imports
 import sys
 import os
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__)) 
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
-from  DataClasses import *
+from PersistentStorage.DBManager import DBManager
+from DataIngestion.DataIngestionMap import DataIngestionMap
+from  DataClasses import Request, Response, DataPoint, Prediction
+from utility import log
+
+from datetime import datetime
+from typing import List, Dict
 
 
 class DataManager():
 
     def make_request(self, request: Request) -> Response:
+        """This method attempts to fill a data request either by gettting the data from the DataBase or from DataIngestion. Will always return a response.
+        ------
+        Parameters:
+            request: Request - A request object filled with the information needed to fulfill the request. (src/DataManagement/DataClasses.py)
+        Returns:
+            Response - A response object holding either the requested data or a error message. (src/DataManagement/DataClasses.py)
+        """
+        try:
+            isPrediction, interval, seriesCode = self.__parse_series(request.series)
+            
+            ###Attempt to pull request from DB
+            dbManager = DBManager()
+            checkedResults = []
+            dbResults = []
+            if isPrediction:
+                dbResults = dbManager.s_prediction_selection(request.source, request.series, request.location, request.fromDateTime, request.toDateTime, request.datum)
+            else:
+                dbResults = dbManager.s_data_point_selection(request.source, request.series, request.location, request.fromDateTime, request.toDateTime, request.datum)
+            
+
+            ###Check contents contains the right amount of results
+
+            #Calculate the amnt of expected results
+            amntExpected = self.__get_amnt_of_results_expected(interval, request.toDateTime, request.fromDateTime)
+            if amntExpected is None:
+                return self.__get_and_log_err_response(request, f'Could not process series, {request.series}, interval value to determin amnt of expected results in request: {request}')
+
+            #First AmountCheck
+            if len(dbResults) != amntExpected:
+
+                #Call Data Ingestion to fetch data
+                dataIngestionMap = DataIngestionMap()
+                diResults = dataIngestionMap.map_fetch(request)
+                if diResults is None:
+                    return self.__get_and_log_err_response(request, f'DB did not have data request, dataIngestion returned NONE, for request: {request}')
+                
+                #Merge data
+                mergedResults = self.__merge_results(dbResults, diResults)
+
+                #Second AmountCheck
+                if(len(mergedResults) != amntExpected):
+                    return self.__get_and_log_err_response(request, f'Merged Data Base Results and Data Ingestion Results failed to have the correct amount of results for request: {request}')
+                else:
+                    checkedResults = mergedResults
+            else:
+                checkedResults = dbResults
+
+            ###Generate proper response object
+            response = Response(request, True)
+            
+            #Splice data down into data objects 
+            try:
+                if isPrediction:
+                    response.data = self.__splice_prediction_results(checkedResults)
+                else:
+                    response.data = self.__splice_dataPoint_results(checkedResults)
+            except Exception as e:
+                return self.__get_and_log_err_response(request, f'An issue occured when attempting to splice returned data into dataObjs for request: {request}\nException: {e}')
+            
+            return response
         
+        except Exception as e:
+            return self.__get_and_log_err_response(request, f'An unknown error occured attempting to fill request: {request}\nException: {e}')
+    
+
+    def __get_and_log_err_response(self, request: Request, msg: str) -> Response:
+        """This function logs an error message as well as generating a Response object with the same message
+        -------
+        Parameters:
+            request: Request - The request that caused the error
+            msg: str - The error message
+        Returns:
+            Response: A response object holding the error information.
+        """
+        log(msg)
+        return Response(request, False, msg)
+    
+
+    def __parse_series(self, series: str) -> tuple:
+        """Parses a series object into its usable parts
+        -------
+        Parameters:
+            series: str - The series to parse.
+        Returns: (tuple)
+            bool - If it is a prediction (true) or data point (false).
+            str - A three char code indicating the interval the data is in.
+            str = A six char unique code of the series.
+        """
+        isPrediction = (True if series[0] == 'p' else False)
+        interval = series[1:3]
+        seriesCode = series[4:]
+        return isPrediction, interval, seriesCode
+    
+    
+    def __get_amnt_of_results_expected(self, interval: str, toDateTime: datetime, fromDateTime: datetime) -> int | None:
+        """Calculates the amount of records we should expect given a time span and an interval code
+        -------
+        Parameters:
+            interval: str - A 3 char interval code.
+            toDateTime: datetime - The late datetime.
+            fromDateTime: datetime - The early datetime.
+        Returns:
+            int | None - Returns the amount of results or none if it can't map the interval to a method
+        """
+        totalSecondsrequested = (toDateTime - fromDateTime).total_seconds()
+        amntOfResultsExpected = None
+        
+        match interval:
+            case '1hr':
+                secondsInInterval = 3600
+            case _:
+                return None
+        
+        amntOfResultsExpected = int(totalSecondsrequested / secondsInInterval)
+        return amntOfResultsExpected
+    
+
+    def __merge_results(self, first: List[Dict], second: List[Dict]) -> List[Dict]:
+        """Merges two lists of dictionaries together, will only keep unique results.
+        -------
+        Parameters:
+            first List[Dict] - The first list to combine.
+            second List[Dict] - The second list to combine.
+        Returns:
+            List[Dict] - The combined, unique List.
+        """
+        uniqueToSecond = second - first
+        return first + uniqueToSecond
+    
+
+    def __splice_prediction_results(self, results: List[Dict]) -> List[Prediction]:
+        """Splices up a list of dbresults, pulling out only the data that changes per point,
+        and places them in a Prediction object.
+        -------
+        Parameters:
+            first List[Dict] - The collection of dbrows.
+        Returns:
+            List[Prediction] - The formatted objs.
+        """
+        predictions = []
+        for row in results:
+            predictions.append(Prediction(
+                row["dataValue"],
+                row["unitsCode"],
+                row["leadTime"],
+                row["timeGenerated"],
+                row["resultCode"]
+            ))
+
+        return predictions
+        
+
+    def __splice_dataPoint_results(self, results: List[Dict]) -> List[DataPoint]:
+        """Splices up a list of dbresults, pulling out only the data that changes per point,
+        and places them in a DataPoint object.
+        -------
+        Parameters:
+            first List[Dict] - The collection of dbrows.
+        Returns:
+            List[Prediction] - The formatted objs.
+        """
+        dataPoints = []
+        for row in results:
+            dataPoints.append(DataPoint(
+                row["dataValue"],
+                row["unitsCode"],
+                row["timeActualized"]
+            ))
+
+        return dataPoints
