0177c714e157212e0693b024532014d8ae8b3679
Savannah Stephenson <stephensonsavannah@ymail.com>
2023-09-20 21:24:42 -0500

    adding header to runner.py file

--- src/Command/runner.py
+++ src/Command/runner.py
@@ -1,1 +1,13 @@
-#how to run semaphore using a message object (kinda like the demo file) 
+# -*- coding: utf-8 -*-
+#message.py
+#----------------------------------
+# Created By: Savannah Stephenson and Anointiyae Beasley
+# Created Date: 9/20/2023
+# version 1.0
+#----------------------------------
+"""This file initiates semaphore for each messageObj passed to it from the RabbitMQ
+ """ 
+#----------------------------------
+# 
+#
+#Imports
