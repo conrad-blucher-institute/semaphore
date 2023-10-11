54d302d6edc7b9a7a6b0568618ae8b2e52eef67a
Beto Estrada Jr <betoejr10@yahoo.com>
2023-10-10 19:14:29 -0500

    Updated Input, SeriesDescription, and Series objects. Added new Time Object. Added a test for Input objects.

--- src/DataClasses.py
+++ src/DataClasses.py
@@ -10,78 +10,46 @@
 #----------------------------------
 # 
 #
-#Imports
 from datetime import datetime
 from typing import List
 
-
-class Actual():
-    """An Actual is a measurement of some environment variable can be linked to a date time.
-        :param value: str - The actual measurements value
-        :param unit: str - the type the value is of
-        :param dateTime: datetime - the time the measurement was taken
+class Input():
+    """An Input is a data value of some environment variable that can be linked to a date time.
+        :param value: str - The actual data value
+        :param unit: str - The unit of measurement of the value
+        :param timeGenerated: datetime - The datetime that the value was created
+        :param timeAcquired: datetime - The datetime that the value was acquired by Semaphore
+        :param timeVerified: datetime - The datetime that the value is valid
         :param longitude: str = None
         :param latitude: str = None
     """
-    def __init__(self, value: str, unit: str, dateTime: datetime, longitude: str = None, latitude: str = None) -> None:
+    def __init__(self, value: str, unit: str, timeAcquired: datetime, timeVerified: datetime, timeGenerated: datetime = None, longitude: str = None, latitude: str = None) -> None:
         self.value = value
         self.unit = unit
-        self.dateTime = dateTime
+        self.timeAcquired = timeAcquired,
+        self.timeVerified = timeVerified,
+        self.timeGenerated = timeGenerated,
         self.longitude = longitude
         self.latitude = latitude
 
     def __str__(self) -> str:
-        return f'\n[Actual] -> value: {self.value}, unit: {self.unit}, dataTime: {self.dateTime}. longitude: {self.longitude}. latitude: {self.latitude}'
+        return f'\n[Input] -> value: {self.value}, unit: {self.unit}, timeGenerated: {self.timeGenerated}, timeAcquired: {self.timeAcquired}, \
+            timeVerified: {self.timeVerified}. longitude: {self.longitude}. latitude: {self.latitude}'
     
     def __repr__(self) -> str:
-        return f'\n[Actual] -> value: {self.value}, unit: {self.unit}, dataTime: {self.dateTime}. longitude: {self.longitude}. latitude: {self.latitude}'
+        return f'\nInput({self.value}, {self.unit}, {self.timeGenerated}, {self.timeAcquired}, {self.timeVerified}, {self.longitude}, {self.latitude})'
     
     def __eq__(self, __value: object) -> bool:
         if (self.value == __value.value and 
             self.unit == __value.unit and 
-            self.dateTime == __value.dateTime and 
+            self.timeGenerated == __value.timeGenerated and
+            self.timeAcquired == __value.timeAcquired and
+            self.timeVerified == __value.timeVerified and
             self.longitude == __value.longitude and 
             self.latitude == __value.latitude):
             return True
         else: return False
 
-class Prediction():
-    """A prediction is a Is a predicted value. It has all the same things as an Actual, but it describes its time as the time the prediction
-    was made and the lead time till its verification time.
-    generatedTime + leadTime = verificationTime
-        :param value: str - The actual measurements value
-        :param unit: str - the type the value is of
-        :param leadTime: float - The time in hours between the generated time and verification time
-        :param generatedTime: datetime - Time the prediction was created
-        :param successValue: str - Planned but not yet used. Its a place to save a confidence value for example.
-        :param longitude = : str = None
-        :param latitude = : str = None
-    """
-    def __init__(self, value: str, unit: str, leadTime: float, generatedTime: datetime, successValue: str = None, longitude: str = None, latitude: str = None) -> None:
-        self.value = value
-        self.unit = unit
-        self.leadTime = leadTime
-        self.generatedTime = generatedTime
-        self.successValue = successValue #Planned but not yet used. Its a place to save a confidence value for example.
-        self.longitude = longitude
-        self.latitude = latitude
-    
-    def __str__(self) -> str:
-        return f'\n[Prediction] -> value: {self.value}, unit: {self.unit}, leadTime: {self.leadTime}, generatedTime: {self.generatedTime}, successValue {self.successValue}. longitude: {self.longitude}. latitude: {self.latitude}'
-    
-    def __repr__(self):
-        return f'\n[Prediction] -> value: {self.value}, unit: {self.unit}, leadTime: {self.leadTime}, generatedTime: {self.generatedTime}, successValue {self.successValue}. longitude: {self.longitude}. latitude: {self.latitude}'
-    
-    def __eq__(self, __value: object) -> bool:
-        if (self.value == __value.value and 
-            self.unit == __value.unit and 
-            self.leadTime == __value.leadTime and 
-            self.generatedTime == __value.generatedTime and 
-            self.successValue == __value.successValue and 
-            self.longitude == __value.longitude and 
-            self.latitude == __value.latitude):
-            return True
-        else: return False
 
 class Output():
     """An output is a a predicted value created by the model semaphore is running. It has all the same things as an Actual, but it describes its time as the time the prediction
@@ -112,36 +80,44 @@ class Output():
             return True
         else: return False
 
+
+class TimeDescription():
+    """A time description should describe the datetime properties of a dataset.
+        :param fromDateTime: datetime - The datetime the data starts at
+        :param toDateTime: datetime - The datetime the data stops at
+        :param interval: int = None - The time step separating the data points in order
+    """
+    def __init__(self, fromDateTime: datetime, toDateTime: datetime, interval: int = None) -> None:
+        self.fromDateTime = fromDateTime,
+        self.toDateTime = toDateTime,
+        self.interval = interval
+
+    def __str__(self) -> str:
+        return f'\n[TimeDescription] -> fromDateTime: {self.fromDateTime}, toDateTime: {self.toDateTime}. interval: {self.interval}'
     
+    def __repr__(self) -> str:
+        return f'\nTimeDescription({self.fromDateTime}, {self.toDateTime}, {self.interval})'
+
 
 class SeriesDescription():
     """A series description should describe a set of data without actually including the data.
-        :param source: str - The data's source Ex. NOAA tides and currents.
-        :param series: str - The series name
-        :param dataClassification: str - actual/prediction/output identifier 
-        :param location: str - The location of the data
-        :param unit: str - The unit (Ex. meter, foot)
-        :param interval: int - The time step separating the datapoints in order
-        :param fromDateTime: datetime - The datetime the data starts at
-        :param toDateTime: datetime - The datetime the data stops at
+        :param source: str - The data's source (e.g. 'NOAATANDC')
+        :param series: str - The series name (e.g. 'x_wind')
+        :param location: str - The location of the data ('packChan')
         :param datum: str = None
     """
-    def __init__(self, source: str, series: str, dataClassification: str, location: str, unit: str, interval: int, fromDateTime: datetime, toDateTime: datetime, datum: str = None) -> None:
+    def __init__(self, source: str, series: str, location: str, datum: str = None) -> None:
         self.source = source
         self.series = series
-        self.dataClassification = dataClassification
         self.location = location
-        self.unit = unit
         self.datum = datum
-        self.interval = interval
-        self.fromDateTime = fromDateTime
-        self.toDateTime = toDateTime
 
     def __str__(self) -> str:
-        return f'\n[SeriesDescription] -> source: {self.source}, series: {self.series}, dataClassification: {self.dataClassification}, location: {self.location}, unit: {self.unit}, datum {self.datum}, interval: {self.interval}, fromDateTime {self.fromDateTime}, toDateTime {self.toDateTime}'
+        return f'\n[SeriesDescription] -> source: {self.source}, series: {self.series}, location: {self.location}. datum {self.datum}'
     
     def __repr__(self) -> str:
-        return f'\n[SeriesDescription] -> source: {self.source}, series: {self.series}, dataClassification: {self.dataClassification}, location: {self.location}, unit: {self.unit}, datum {self.datum}, interval: {self.interval}, fromDateTime {self.fromDateTime}, toDateTime {self.toDateTime}'
+        return f'\nSeriesDescription({self.source}, {self.series}, {self.location}, {self.datum})'
+
 
 class SemaphoreSeriesDescription():
     """A semaphore series description should describe a set of predictions that is actually generated by semaphore.
@@ -172,15 +148,18 @@ class SemaphoreSeriesDescription():
     def __repr__(self) -> str:
         return f'\n[SemaphoreSeriesDescription] -> AIName: {self.ModelName}, AIGeneratedVersion: {self.ModelVersion}, location: {self.location}, interval: {self.interval}, fromDateTime {self.fromDateTime}, toDateTime {self.toDateTime}, leadTime {self.leadTime}, datum: {self.datum}'
 
+
 class Series():
     """A series is a pairing of an object describing a series of data, and an array of the data itself. It also includes some meta data about how well the data provided matches the description.
-        :param description: SeriesDescription | SemaphoreSeriesDescription - An object that will describe to you what unique data set this is.
-        :param isComplete: bool - Weather the description object completely describes the bound data.
+        :param description: SeriesDescription | SemaphoreSeriesDescription - An object that will describe to you what unique data set this is
+        :param isComplete: bool - Whether the description object completely describes the bound data
+        :param timeDescription: TimeDescription = None - An object that contains the datetime properties of the data
         :param nonCompleteReason: Exception = None - The exception that cased the problem
     """
-    def __init__(self, description: SeriesDescription | SemaphoreSeriesDescription, isComplete: bool, nonCompleteReason: Exception = None) -> None:
+    def __init__(self, description: SeriesDescription | SemaphoreSeriesDescription, isComplete: bool, timeDescription: TimeDescription = None, nonCompleteReason: Exception = None) -> None:
         self.description = description
         self.isComplete = isComplete
+        self.timeDescription = timeDescription
         self.nonCompleteReason = nonCompleteReason
         self.__data = []
 
@@ -189,9 +168,9 @@ class Series():
         return self.__data
     
     @data.setter
-    def data(self, data: List[Actual | Prediction]) -> None:
+    def data(self, data: List[Input | Output]) -> None:
         self.__data = data
 
     def __str__(self) -> str:
-        return f'\n[Series] -> description: {self.description}, wasSuccessful: {self.isComplete}, errorReason: {self.nonCompleteReason}'
+        return f'\n[Series] -> description: {self.description}, wasSuccessful: {self.isComplete}, timeDescription: {self.timeDescription}, errorReason: {self.nonCompleteReason}'
     
