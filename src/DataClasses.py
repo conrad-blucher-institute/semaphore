7b87fd754c8b2697169ec3171d4183b2bc2a1cb4
Savannah Stephenson <stephensonsavannah@ymail.com>
2023-10-11 21:50:22 -0500

    renaming to match schema

--- src/DataClasses.py
+++ src/DataClasses.py
@@ -5,7 +5,7 @@
 # Created Date: 4/30/2023
 # version 2.0
 #----------------------------------
-"""This file holds for classes that the system will use to describes series of data, actuals, and predictions.
+"""This file holds for classes that the system will use to describes inputs and outputs.
  """ 
 #----------------------------------
 # 
@@ -15,26 +15,26 @@ from typing import List
 
 class Input():
     """An Input is a data value of some environment variable that can be linked to a date time.
-        :param value: str - The actual data value
-        :param unit: str - The unit of measurement of the value
+        :param dataValue: str - The actual data value
+        :param dataUnit: str - The unit of measurement of the value
         :param longitude: str = None
         :param latitude: str = None
     """
-    def __init__(self, value: str, unit: str, longitude: str = None, latitude: str = None) -> None:
-        self.value = value
-        self.unit = unit
+    def __init__(self, dataValue: str, dataUnit: str, longitude: str = None, latitude: str = None) -> None:
+        self.dataValue = dataValue
+        self.dataUnit = dataUnit
         self.longitude = longitude
         self.latitude = latitude
 
     def __str__(self) -> str:
-        return f'\n[Input] -> value: {self.value}, unit: {self.unit}, longitude: {self.longitude}. latitude: {self.latitude}'
+        return f'\n[Input] -> value: {self.dataValue}, unit: {self.dataUnit}, longitude: {self.longitude}. latitude: {self.latitude}'
     
     def __repr__(self) -> str:
-        return f'\nInput({self.value}, {self.unit}, {self.longitude}, {self.latitude})'
+        return f'\nInput({self.dataValue}, {self.dataUnit}, {self.longitude}, {self.latitude})'
     
     def __eq__(self, __value: object) -> bool:
-        if (self.value == __value.value and 
-            self.unit == __value.unit and 
+        if (self.dataValue == __value.dataValue and 
+            self.dataUnit == __value.dataUnit and 
             self.longitude == __value.longitude and 
             self.latitude == __value.latitude):
             return True
@@ -43,28 +43,28 @@ class Input():
 
 class Output():
     """An output is a a predicted value created by the model semaphore is running.
-        :param value: str - The actual data value
-        :param unit: str - The unit of measurement of the value
+        :param dataValue: str - The actual data value
+        :param dataUnit: str - The unit of measurement of the value
         :param timeGenerated: datetime - The datetime that the value was created
         :param longitude: str = None
         :param latitude: str = None
     """
-    def __init__(self, value: str, unit: str, timeGenerated: datetime, longitude: str = None, latitude: str = None) -> None:
-        self.value = value
-        self.unit = unit
+    def __init__(self, dataValue: str, dataUnit: str, timeGenerated: datetime, longitude: str = None, latitude: str = None) -> None:
+        self.dataValue = dataValue
+        self.dataUnit = dataUnit
         self.timeGenerated = timeGenerated
         self.latitude = latitude
         self.longitude = longitude
         
     def __str__(self) -> str:
-        return f'\n[Output] -> value: {self.value}, unit: {self.unit}, timeGenerated: {self.timeGenerated}, longitude: {self.longitude},latitude: {self.latitude}'
+        return f'\n[Output] -> value: {self.dataValue}, unit: {self.dataUnit}, timeGenerated: {self.timeGenerated}, longitude: {self.longitude},latitude: {self.latitude}'
     
     def __repr__(self):
-        return f'\nOutput({self.value},{self.unit},{self.timeGenerated},{self.longitude},{self.latitude})'
+        return f'\nOutput({self.dataValue},{self.dataUnit},{self.timeGenerated},{self.longitude},{self.latitude})'
     
     def __eq__(self, __value: object) -> bool:
-        if (self.value == __value.value and 
-            self.unit == __value.unit and  
+        if (self.dataValue == __value.dataValue and 
+            self.dataUnit == __value.dataUnit and  
             self.timeGenerated == __value.timeGenerated and
             self.longitude == __value.longitude and 
             self.latitude == __value.latitude):
@@ -92,44 +92,44 @@ class TimeDescription():
 
 class SeriesDescription():
     """A series description should describe a set of data without actually including the data.
-        :param source: str - The data's source (e.g. 'NOAATANDC')
-        :param series: str - The series name (e.g. 'x_wind')
-        :param location: str - The location of the data ('packChan')
-        :param datum: str = None
+        :param dataSource: str - The data's source (e.g. 'NOAATANDC')
+        :param dataSeries: str - The series name (e.g. 'x_wind')
+        :param dataLocation: str - The location of the data ('packChan')
+        :param dataDatum: str = None
     """
-    def __init__(self, source: str, series: str, location: str, datum: str = None) -> None:
-        self.source = source
-        self.series = series
-        self.location = location
-        self.datum = datum
+    def __init__(self, dataSource: str, dataSeries: str, dataLocation: str, dataDatum: str = None) -> None:
+        self.dataSource = dataSource
+        self.dataSeries = dataSeries
+        self.dataLocation = dataLocation
+        self.dataDatum = dataDatum
 
     def __str__(self) -> str:
-        return f'\n[SeriesDescription] -> source: {self.source}, series: {self.series}, location: {self.location}. datum {self.datum}'
+        return f'\n[SeriesDescription] -> source: {self.dataSource}, series: {self.dataSeries}, location: {self.dataLocation}. datum {self.dataDatum}'
     
     def __repr__(self) -> str:
-        return f'\nSeriesDescription({self.source}, {self.series}, {self.location}, {self.datum})'
+        return f'\nSeriesDescription({self.dataSource}, {self.dataSeries}, {self.dataLocation}, {self.dataDatum})'
 
 
 class SemaphoreSeriesDescription():
     """A semaphore series description should describe a set of predictions that is actually generated by semaphore.
-        :param ModelName: str - The name of the model
-        :param ModelVersion: str - The version of the model
-        :param location: str - The data's location
+        :param modelName: str - The name of the model
+        :param modelVersion: str - The version of the model
+        :param dataLocation: str - The data's location
         :param leadTime: timedelta - The time in hours between the generated time and verification time
-        :param datum: str = None
+        :param dataDatum: str = None
     """
-    def __init__(self, ModelName: str, ModelVersion: str, location: str, leadTime: timedelta, datum: str = None) -> None:
-        self.ModelName = ModelName
-        self.ModelVersion = ModelVersion
-        self.location = location
+    def __init__(self, modelName: str, modelVersion: str, dataLocation: str, leadTime: timedelta, dataDatum: str = None) -> None:
+        self.modelName = modelName
+        self.modelVersion = modelVersion
+        self.dataLocation = dataLocation
         self.leadTime = leadTime
-        self.datum = datum
+        self.dataDatum = dataDatum
 
     def __str__(self) -> str:
-        return f'\n[SemaphoreSeriesDescription] -> AIName: {self.ModelName}, AIGeneratedVersion: {self.ModelVersion}, location: {self.location}, leadTime {self.leadTime}, datum: {self.datum}'
+        return f'\n[SemaphoreSeriesDescription] -> AIName: {self.modelName}, AIGeneratedVersion: {self.modelVersion}, location: {self.dataLocation}, leadTime {self.leadTime}, datum: {self.dataDatum}'
     
     def __repr__(self) -> str:
-        return f'\nSemaphoreSeriesDescription({self.ModelName},{self.ModelVersion}, {self.location},{self.leadTime},{self.datum})'
+        return f'\nSemaphoreSeriesDescription({self.modelName}, {self.modelVersion}, {self.dataLocation}, {self.leadTime}, {self.dataDatum})'
 
 
 class Series():
