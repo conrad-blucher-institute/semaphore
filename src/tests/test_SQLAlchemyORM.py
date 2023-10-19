c9a1514b9bb85b44d46feb25d8454fdbb87f5d09
matdenkas <mateokastl@gmail.com>
2023-10-18 23:17:40 -0500

    Finallizing  tests

--- src/tests/test_SQLAlchemyORM.py
+++ src/tests/test_SQLAlchemyORM.py
@@ -1,47 +1,30 @@
+# -*- coding: utf-8 -*-
+#test_SQLAlchemyORM.py
+#-------------------------------
+# Created By : Savannah Stephenson
+# Created Date: 10/18/2023
+# version 1.0
+#-------------------------------
+""" This file is an implementation of the SQLAlchemy ORM geared towards Semaphore and its schema. 
+ """ 
+#-------------------------------
+# 
+#
+#Imports
 import sys
 import os
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
-from datetime import datetime, timedelta
-from DataClasses import Input, Output, Series,TimeDescription,SeriesDescription
+from datetime import datetime, timedelta, time
+from DataClasses import Input, Output, Series, TimeDescription, SeriesDescription, SemaphoreSeriesDescription
 from SeriesStorage.SeriesStorage.SQLAlchemyORM import SQLAlchemyORM
 from dotenv import load_dotenv
-
-def test_insert_input():
-    sqlorm = SQLAlchemyORM()
-    seriesD = SeriesDescription('hi','bye','goodbye')
-    sqlorm.create_DB()
-    currentDate = datetime.now()
-    pastDate = currentDate - timedelta(days=7)
-
-    input = Input(value = '1.028', 
-                  unit = 'meter', 
-                  timeGenerated = pastDate, 
-                  timeAcquired = currentDate, 
-                  timeVerified = pastDate,
-                  longitude = '-97.318',
-                  latitude = '27.4844')
-    series = Series(seriesD, True)
-    series.data = [input]
-    actualResults = sqlorm.insert_input(series)
-    assert True
-    
-def test_insert_output():
-    sqlorm = SQLAlchemyORM()
-    seriesD = SeriesDescription('hi','bye','goodbye')
-    sqlorm.create_DB()
-    currentDate = datetime.now()
-    timeD= timedelta(days=7)
-
-    output = Output('1.028', '2.00', currentDate, timeD)
-    series = Series(seriesD, True)
-    series.data = [output]
-    actualResults = sqlorm.insert_output(series)
-    assert True
     
 
 def test_find_lat_lon():
+    """This test tests the ORMs ability to insert data into the location reference table, and pull it back
+    """
     load_dotenv()
     sqlorm = SQLAlchemyORM()
     sqlorm.drop_DB()
@@ -51,6 +34,8 @@ def test_find_lat_lon():
     assert result == ('-73', '32')
 
 def test_find_external_location_code():
+    """This test tests the ORMs ability to insert data into the db's mapping table, and pull it back
+    """
     load_dotenv()
     sqlorm = SQLAlchemyORM()
     sqlorm.drop_DB()
@@ -58,3 +43,60 @@ def test_find_external_location_code():
     sqlorm.insert_external_location_code('PicinicBasket', 'sourceCode', 'dataSourcelocationCode', 1 )
     result = sqlorm.find_external_location_code('sourceCode', 'PicinicBasket', 1)
     assert result == 'dataSourcelocationCode'
+
+def test_input_table():
+    """This test tests the ORMs ability to insert data into the db's input table, and pull it back
+    """
+    #Load fresh DB
+    load_dotenv()
+    sqlorm = SQLAlchemyORM()
+    sqlorm.drop_DB()
+    sqlorm.create_DB()
+
+    now = datetime.now()
+
+    # Create needed data objects
+    seriesDescription = SeriesDescription('apple', 'banana', 'orange', 'grape')
+    timeDescription = TimeDescription(now, now)
+    input = Input('pear', 'cherry', now, now, '12', '12')
+
+    series = Series(seriesDescription, True, timeDescription)
+    series.data = [input]
+
+    # Insert, select, and check
+    sqlorm.insert_input(series)
+    result = sqlorm.select_input(seriesDescription, timeDescription)
+
+    assert result.data == series.data
+
+def test_output_table():
+    """This test tests the ORMs ability to insert data into the db's output table, and pull it back
+    """
+    #Load fresh DB
+    load_dotenv()
+    sqlorm = SQLAlchemyORM()
+    sqlorm.drop_DB()
+    sqlorm.create_DB()
+
+    now = datetime.now()
+
+    # Create needed data objects
+    seriesDescription =  SemaphoreSeriesDescription('apple', 'banana', 'orange', 'strawberry', 'pineapple')
+    timeDescription = TimeDescription(now, now)
+    leadTime = timedelta(1, 1, 1, 1)
+    input = Output('pear', 'cherry', now, leadTime)
+
+    series = Series(seriesDescription, True, timeDescription)
+    series.data = [input]
+
+    # Insert, select, and check
+    sqlorm.insert_output(series)
+    
+    # Adjust from and to time to format like a request (verification time)
+    timeDescription.fromDateTime += leadTime
+    timeDescription.toDateTime += leadTime
+
+    result = sqlorm.select_output(seriesDescription, timeDescription)
+
+    assert result.data == series.data
+
