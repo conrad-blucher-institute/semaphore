24c6328e6fce2ec2bb265d724de1c5c5088eb578
Beto Estrada Jr <betoejr10@yahoo.com>
2023-10-24 20:28:33 -0500

    Okay now I fixed the headers

--- src/tests/test_InputGatherer.py
+++ src/tests/test_InputGatherer.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 #test_InputGatherer.py
 #-------------------------------
-# Created By : Beto Estrada
+# Created By: Beto Estrada
 # Created Date: 10/24/2023
 # version 1.0
-#-------------------------------
+#----------------------------------
+"""This file tests the InputGatherer module
+ """ 
+#----------------------------------
+# 
+#
 import sys
 import os
 from os import path, getenv
