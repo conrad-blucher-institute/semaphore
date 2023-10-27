8d2b8576d2ac487e614cf2f00c5826e1df0238a0
Joy <anointiyaebeasley1722@gmail.com>
2023-10-27 08:39:05 -0500

    Refactor ModelWrapper

--- src/ModelExecution/IOutputHandler.py
+++ src/ModelExecution/IOutputHandler.py
@@ -17,24 +17,25 @@ import os
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__)) 
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
-from DataClasses import Prediction, SemaphoreSeriesDescription, Series
+from DataClasses import SemaphoreSeriesDescription, Series
 from utility import log
 
 from abc import ABC, abstractmethod
 from importlib import import_module
-
+from dspec import Dspec
 class IOutputHandler(ABC):
-
+   
     @abstractmethod
-    def post_process_prediction(self, predictionDesc: SemaphoreSeriesDescription, prediction: Prediction) -> Series:
+    def post_process_prediction(self, prediction: list[any], dspec:Dspec ) -> Series:
         raise NotImplementedError()
              
-
+       
 
 def output_handler_factory(method: str) -> IOutputHandler:
     """Uses the source attribute of a data request to dynamically import a module
     ------
     Parameters
+        method:
         method: str - the string name for the outputHandler class
     Returns
         IOutputHandler - An child of the IOutputHandler interface.
