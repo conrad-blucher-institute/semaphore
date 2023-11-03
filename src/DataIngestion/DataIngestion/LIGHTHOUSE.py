2b689373ea96c5fb8c0c49fbb4a3364cf88dbbfd
matdenkas <mateokastl@gmail.com>
2023-11-02 22:23:33 -0500

    Lighthouse ingestion class

--- src/DataIngestion/DataIngestion/LIGHTHOUSE.py
+++ src/DataIngestion/DataIngestion/LIGHTHOUSE.py
@@ -23,34 +23,35 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 from src.SeriesStorage.ISeriesStorage import series_storage_factory
-from DataClasses import Series, SeriesDescription, Actual, Prediction
+from DataClasses import Series, SeriesDescription, Input, TimeDescription
 from DataIngestion.IDataIngestion import IDataIngestion
 from utility import log
 
 from datetime import datetime
 from urllib.error import HTTPError
 from urllib.request import urlopen
-from math import radians, cos, sin
 import json
-
-from typing import List, Dict
 
 
 class LIGHTHOUSE(IDataIngestion):
+
     
     def __init__(self):
         self.seriesStorage = series_storage_factory()
+        self.lighthouseDataInfoMap = {'dAirTmp': ('atp', 'celsius'),
+                                       'dWaterTmp': ('wtp', 'celsius')}
 
     
-    def ingest_series(self, seriesDescription: SeriesDescription) -> Series | None:
-        return self.__pull_pd_endpoint(seriesDescription)
-    #dAirTmp
-    #dWaterTmp
+    def ingest_series(self, seriesDescription: SeriesDescription, timeDescription: TimeDescription) -> Series | None:
+        #Right now only implemented for data points
+        return self.__pull_pd_endpoint_dataPoint(seriesDescription, timeDescription)
     
 
     def __api_request(self, url: str) -> None | dict:
-        """Given the parameters, generates and utilize a url to hit the t&C api. 
+        """Given a url, this function attempts to hit this URL and download the response as a JSON. 
         NOTE On a bad api param, throws urlib HTTPError, code 400
+         :param url: str - The url to hit
+         :returns data: json-like dict - the data that was downloaded
         """
        
         try: #Attempt download
@@ -65,25 +66,65 @@ class LIGHTHOUSE(IDataIngestion):
             return None
         return data
 
-    def __pull_pd_endpoint(self, seriesDescription: SeriesDescription) -> Series | None:
+    def __pull_pd_endpoint_dataPoint(self, seriesDescription: SeriesDescription, timeDescription: TimeDescription) -> Series | None:
         
-        fromString = seriesDescription.fromDateTime.strftime('%m/%d/%y').replace('/', '%2F')
-        toString = seriesDescription.toDateTime.strftime('%m/%d/%y').replace('/', '%2F')
-
+        # Reformat and sterilize datetimes
+        fromString = timeDescription.fromDateTime.strftime('%m/%d/%y').replace('/', '%2F')
+        toString = timeDescription.toDateTime.strftime('%m/%d/%y').replace('/', '%2F')
         
-        url = f'http://lighthouse.tamucc.edu/pd?stnlist=013&serlist=atp&when={fromString}%2C{toString}&whentz=UTC0&-action=app_json&unit=metric&elev=stnd'
+        ### Find what LIGHTHOUSE calls the series we want
+        # sim = series info map
+        SIMSeriesCodeIndex = 0
+        SIMSeriesUnitIndex = 1
+        sim = self.lighthouseDataInfoMap.get(seriesDescription.dataSeries)
+        if(sim == None):
+            log(f'No mapping for series code {seriesDescription.dataSeries} found in LIGHTHOUSE ingestion class!')
+            return None
+        
+        ### Find what LIGHTHOUSE calls the location we want
+        lighthouseLocationCode = self.seriesStorage.find_external_location_code(seriesDescription.dataSource, seriesDescription.dataLocation)
+        
+        ### Get LIGHTHOUSE data
+        # Build the URL to hit
+        url = f'http://lighthouse.tamucc.edu/pd?stnlist={lighthouseLocationCode}&serlist={sim[SIMSeriesCodeIndex]}&when={fromString}%2C{toString}&whentz=UTC0&-action=app_json&unit=metric&elev=stnd'
         apiReturn = self.__api_request(url)
+        if apiReturn == None:
+            log(f'LIGHTHOUSE | __pull_pd_endpoint_dataPoint | For unknown reason fetch failed for {seriesDescription}{timeDescription}')
+            return None
 
-        print('\n\n\n\n\n\n\n\n\n\n\n\n')
-        print(apiReturn)
-        print(apiReturn['013']['data']['atp'])
+        # Parse Meta Data
+        lat = apiReturn['013']['lat']
+        lon = apiReturn['013']['lon']
+        data = apiReturn[lighthouseLocationCode]['data'][sim[SIMSeriesCodeIndex]]
 
-        data = apiReturn['013']['data']['atp']
-        for datapoint in data
-        print('\n\n\n\n\n\n\n\n\n\n\n\n')
-        assert False
+        ### Convert data to a list of inputs
+        dataValueIndex = 1
+        dataTimestampIndex = 0
+        inputs = []
+        for dataPoint in data:
+            if(dataPoint[dataValueIndex] != None): # If lighthouse does not have a requested value, it will return None
+                
+                #Filter data via interval if one was provided
+                # Lighthouse returns epoch time in milliseconds
+                epochTimeStamp = dataPoint[dataTimestampIndex]/1000
+                if timeDescription.interval != None:
+                    if epochTimeStamp % timeDescription.interval.total_seconds() != 0:
+                        continue
 
-        # url = %2F
+                dt = datetime.utcfromtimestamp(epochTimeStamp)
+                inputs.append(Input(
+                    dataPoint[dataValueIndex],
+                    sim[SIMSeriesUnitIndex],
+                    dt, # Validation time and generated time are the same as these are data points
+                    dt,
+                    lon,
+                    lat
+                ))
 
-# 9/28/23, +4h
-# http://lighthouse.tamucc.edu/pd?stnlist=013&serlist=atp&when=9%2F28%2F23%2C-1h&whentz=UTC0&-action=app_json&unit=metric&elev=stnd
+        ### Build Series, send data to DB, return data
+        resultSeries = Series(seriesDescription, timeDescription)
+        resultSeries.data = inputs
+        
+        #Insert series into DB
+        self.seriesStorage.insert_input(resultSeries)
+        return resultSeries
