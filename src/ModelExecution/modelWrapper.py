f6f7ef85008b36d528c5ba667d890ea4914c4c43
Beto Estrada Jr <betoejr10@yahoo.com>
2023-12-09 11:28:15 -0600

    Now passing referenceTime to post_process_predition

--- src/ModelExecution/modelWrapper.py
+++ src/ModelExecution/modelWrapper.py
@@ -121,7 +121,7 @@ class ModelWrapper:
     
             #Instantiate the right output handler method then post process the predictions
             OH_Class = output_handler_factory(outputInfo.outputMethod)
-            processedOutputs = OH_Class.post_process_prediction(prediction, dspec) 
+            processedOutputs = OH_Class.post_process_prediction(prediction, dspec, dateTime) 
 
             #Put the post processed predictions in a series
             series = Series(predictionDesc, True)
