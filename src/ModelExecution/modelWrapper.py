b1e0e96975d38c5f8d38558d2a297f994ffed156
matdenkas <mateokastl@gmail.com>
2024-01-03 14:47:54 -0600

    FIXING MLP Co-authored-by: Beto Estrada Jr <bestrada33@users.noreply.github.com> Co-authored-by: Savannah <lovelysandlonelys@users.noreply.github.com>

--- src/ModelExecution/modelWrapper.py
+++ src/ModelExecution/modelWrapper.py
@@ -75,9 +75,19 @@ class ModelWrapper:
         #Get only first and last layers
         firstLayer, *_, lastLayer = self._model.layers 
 
+        shape = firstLayer.input_shape
+        if type(shape) == list:
+            shape = shape[0]
+
+        if type(shape) != tuple:
+            raise TypeError(f'First layer input shape was not a tuple, instead: {type(shape)}')
+        
+        if int(shape[-1]) != len(inputs):
+            raise ValueError(f'Incorrect amount of inputs to shape. Desired: {shape[-1]} Received: {len(inputs)}')
+
         #Karas reports the Batch size as None in the shape; Convert None -> 1, 
         #We only want one prediction as opposed to a batch.
-        shapeTarget = [int(1 if value is None else value) for value in firstLayer.input_shape]
+        shapeTarget = [int(1 if value is None else value) for value in shape]
 
         #Reshape and return the data.
         return reshape(inputs, shapeTarget) 
@@ -88,12 +98,17 @@ class ModelWrapper:
         """
         #converting execution time to reference time
         referenceTime = self.__inputGatherer.calculate_referenceTime(dateTime)
-        
+            
         try:
             inputs = self.__inputGatherer.get_inputs(referenceTime)
             if inputs == -1: return -1
             
-            shapedInputs = self.__shape_data(inputs) #Ensure received inputs are shaped right for model
+            try:
+                shapedInputs = self.__shape_data(inputs) #Ensure received inputs are shaped right for model
+            except Exception as e:
+                log(e)
+                return -1
+            
             return self._model.predict(shapedInputs) 
         except Exception as e:
             log(e)
@@ -105,37 +120,33 @@ class ModelWrapper:
         #converting execution time to reference time
         referenceTime = self.__inputGatherer.calculate_referenceTime(dateTime)
         
+
+        inputs = self.__inputGatherer.get_inputs(referenceTime)
+        if len(inputs) == 0: 
+            log('No inputs received for model.')
+            return -1
+
         try:
-            inputs = self.__inputGatherer.get_inputs(referenceTime)
-            if len(inputs) == 0: 
-                log('No inputs received for model.')
-                return -1
-
-            try:
-                shapedInputs = self.__shape_data(inputs) #Ensure received inputs are shaped right for model
-            except:
-                log('Shaping failure, likely caused by not receiving enough inputs.')
-                return -1
-
-            prediction =  self._model.predict(shapedInputs) 
-            dspec = self.__inputGatherer.get_dspec()
-            outputInfo = dspec.outputInfo
-
-            predictionDesc = SemaphoreSeriesDescription(dspec.modelName, dspec.modelVersion, outputInfo.series, outputInfo.location, outputInfo.datum)
-    
-            #Instantiate the right output handler method then post process the predictions
-            OH_Class = output_handler_factory(outputInfo.outputMethod)
-            processedOutputs = OH_Class.post_process_prediction(prediction, dspec, dateTime) 
-
-            #Put the post processed predictions in a series
-            series = Series(predictionDesc, True)
-            series.data = processedOutputs
-
-            #Send the prediction to the database and return the result
-            SS_Class = series_storage_factory()
-            result = SS_Class.insert_output(series)
-            return result
-
+            shapedInputs = self.__shape_data(inputs) #Ensure received inputs are shaped right for model
         except Exception as e:
             log(e)
             return -1
+
+        prediction =  self._model.predict(shapedInputs) 
+        dspec = self.__inputGatherer.get_dspec()
+        outputInfo = dspec.outputInfo
+
+        predictionDesc = SemaphoreSeriesDescription(dspec.modelName, dspec.modelVersion, outputInfo.series, outputInfo.location, outputInfo.datum)
+
+        #Instantiate the right output handler method then post process the predictions
+        OH_Class = output_handler_factory(outputInfo.outputMethod)
+        processedOutputs = OH_Class.post_process_prediction(prediction, dspec, dateTime) 
+
+        #Put the post processed predictions in a series
+        series = Series(predictionDesc, True)
+        series.data = processedOutputs
+
+        #Send the prediction to the database and return the result
+        SS_Class = series_storage_factory()
+        result = SS_Class.insert_output(series)
+        return result
