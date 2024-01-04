1d08577ec23863b22fd393199744972c05adf8c2
Savannah Stephenson <stephensonsavannah@ymail.com>
2024-01-04 14:18:12 -0600

    adding subprocess commands

--- tools/init_cron.py
+++ tools/init_cron.py
@@ -5,15 +5,23 @@ sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 import subprocess
 
-#check if cron file already exists
+load_dotenv()
 
-#if not create a cron file on the machine
+def main():
+    #read the dspec folder to create a cron job for each model 
+    #checking that dspec file passed exists in the dspec folder
+    dspecFilePath = construct_true_path(getenv('DSPEC_FOLDER_PATH'))
 
-#read the dspec folder to create a cron job for each model 
 
-    # until no more dspec files 
-    # find interval and convert to cron time
-    # find despec file name 
-    # construct shell text ex:  10 * * * * docker exec -d core python3 src/semaphoreRunner.py -d test_dspec.json
+        # until no more dspec files 
+        # find interval and convert to cron time
+        # find despec file name 
+        # construct shell text ex:  10 * * * * docker exec -d core python3 src/semaphoreRunner.py -d test_dspec.json
 
-#add all shell text to the cron file
+    #if not create a cron file on the machine
+    subprocess.run('crontab', '-l', 'mycron')
+
+    #add all shell text to the cron file
+    subprocess.run('echo', '"" >> mycron')
+    subprocess.run('crontab', 'mycron')
+    subprocess.run('rm', 'mycron')
