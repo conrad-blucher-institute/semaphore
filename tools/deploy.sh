8bf26c5ad215f8c809f31d2efc026a2a9a259966
matdenkas <mateokastl@gmail.com>
2024-02-14 12:50:21 -0600

    v5

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -3,5 +3,5 @@ docker-compose down
 git pull
 docker-compose build
 docker-compose up -d
-docker exec semaphore-core python3 tools/init_cron.py 
+python3 tools/init_cron.py 
 docker exec semaphore-core python3 tools/init_db.py 
