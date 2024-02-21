59a19c37f7c31b60bdebc1a0bcb4ddf3e7b9f2b9
Savannah Stephenson <stephensonsavannah@ymail.com>
2024-02-21 14:47:35 -0600

    i tried like a million things this is the thing still in the works

---
+++ apiHealthCheck.sh
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#apiDriver.py
+#----------------------------------
+# Created By: Savannah Stephenson
+# Created Date: 02/21/2004
+#----------------------------------
+
+curl -f http://localhost:8888/health
