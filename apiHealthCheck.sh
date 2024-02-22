58a533567accd2a69f3dee239a272612742f3262
Savannah Stephenson <stephensonsavannah@ymail.com>
2024-02-21 18:40:33 -0600

    using host.docker.interal as host name

--- apiHealthCheck.sh
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-#apiDriver.py
-#----------------------------------
-# Created By: Savannah Stephenson
-# Created Date: 02/21/2004
-#----------------------------------
-
-curl -f http://localhost:8888/health
