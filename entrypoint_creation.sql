760871c3a46957ed3319ad2eda07f8505ec779a2
Savannah Stephenson <stephensonsavannah@ymail.com>
2024-04-03 03:29:11 -0500

    comment

--- entrypoint_creation.sql
+++ entrypoint_creation.sql
@@ -2,6 +2,9 @@
 
 -- Grant privileges to core user on existing database
 CREATE USER ${CORE_USER} WITH PASSWORD '${CORE_PASSWORD}';
+-- The ISSUE right now is that we can't read these: ${CORE_USER} from the .env file
+-- because sql isn't built like that, so the current solution idea is to write a preprocess
+-- script but- i'm kinda confused on it
 GRANT CONNECT ON DATABASE ${POSTGRES_DB} TO ${CORE_USER};
 GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA public TO ${CORE_USER};
 GRANT CREATE ON SCHEMA public TO ${CORE_USER};
