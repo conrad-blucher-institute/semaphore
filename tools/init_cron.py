8ed5b231892869eaba344f3c83af5686505ab5b6
Joy <anointiyaebeasley1722@gmail.com>
2024-04-12 20:29:17 -0500

    testing logs directory

--- tools/init_cron.py
+++ tools/init_cron.py
@@ -18,12 +18,23 @@ def main():
         # find despec file name 
         # construct shell text ex:  10 * * * * docker exec -d core python3 src/semaphoreRunner.py -d test_dspec.json
         
-        
-    directory = "../logs" #Places logs in the root path
+    # Get the current working directory
+    current_directory = os.getcwd()
     
-    if not os.path.exists(directory):
-     os.makedirs(directory)
+    # Get the parent directory
+    parent_directory = os.path.dirname(current_directory)
     
+    # Define the path for the logs directory
+    logs_directory = os.path.join(parent_directory, "logs")
+    
+    # Check if the logs directory already exists
+    if not os.path.exists(logs_directory):
+        # Create the logs directory
+        os.makedirs(logs_directory)
+        print(f"Logs directory created at: {logs_directory}")
+    else:
+        print(f"Logs directory already exists at: {logs_directory}")
+
     # Clear out the cron file SAFELY
     subprocess.run(['crontab', '-r'])
     # Make a new cron file
