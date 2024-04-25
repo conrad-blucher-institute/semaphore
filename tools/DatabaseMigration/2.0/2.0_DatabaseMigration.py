b5dc40173555888ce6274c7f12b5835da0dbfd48
Savannah Stephenson <stephensonsavannah@ymail.com>
2024-04-25 15:54:46 -0500

    filling in funcitons for 2.0

--- tools/DatabaseMigration/2.0/2.0_DatabaseMigration.py
+++ tools/DatabaseMigration/2.0/2.0_DatabaseMigration.py
@@ -15,7 +15,9 @@
 #
 #Imports
 from IDatabaseMigration import IDatabaseMigration
-from sqlalchemy import Engine
+from sqlalchemy import Table, Column, Integer, String, DateTime, MetaData, Engine
+from sqlalchemy.dialects.postgresql import insert
+import datetime
 
 
 class Migrator(IDatabaseMigration):
@@ -26,7 +28,43 @@ class Migrator(IDatabaseMigration):
            :param databaseEngine: Engine - the engine of the database we are connecting to (semaphore)
            :return: bool indicating successful update
         """
-        pass
+        #create the schema of the additional table 
+        self.__create_schema()
+
+        #build dictionary with first row information
+        first_row_dict = [{'versionNumber':'2.0', 'migrationTime':datetime.utcnow(), 'versionNotes': " "}]
+        
+        #starting a transaction
+        with databaseEngine.begin() as connection:
+            #add new table to the database
+            self._metadata.create_all(connection)
+            #insert the first row
+            connection.execute(insert(self.deployed_database_version)
+                               .value(first_row_dict))
+            connection.commit()
+        
+        return True
+
+    def __create_schema(self) -> None:
+        """Builds the db schema for the version table in the metadata.
+        """
+
+        self._metadata = MetaData()
+        
+        #this table stores the current version of the database
+        self.deployed_database_version = Table(
+            "deployed_database_version",
+            self._metadata,
+
+            Column("id", Integer, autoincrement=True, primary_key=True),
+
+            Column("versionNumber", String(10), nullable=False),
+
+            Column("migrationTime", DateTime, nullable=False),
+
+            Column("versionNotes", String(255), nullable=False)
+        )
+        
 
     def rollback(self, databaseEngine: Engine) -> bool:
         """This function rolls the database back to version 1.0 which involves
@@ -34,4 +72,14 @@ class Migrator(IDatabaseMigration):
            :param databaseEngine: Engine - the engine of the database we are connecting to (semaphore)
            :return: bool indicating successful update
         """
-        pass
+
+        #setting engine
+        self.__engine = databaseEngine
+
+         #starting a transaction
+        with databaseEngine.begin() as connection:
+            #dropping the table
+            self.deployed_database_version.drop(connection)
+
+        return True
+        
