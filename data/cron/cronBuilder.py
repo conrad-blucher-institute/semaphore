cc30c6c6ae5da436b5e45308c4b4f769d416adec
Joy <anointiyaebeasley1722@gmail.com>
2024-05-21 13:31:26 -0500

    cron buiilder plan

---
+++ data/cron/cronBuilder.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# cronBuilder.py
+#----------------------------------
+# Created By: Op Team
+# Created Date: 5/21/2024
+# version 1.0
+#----------------------------------
+"""This module will automate the building of the cron file.
+ """ 
+#----------------------------------
+# 
+#
+#Imports
+
+#def cron builder
+    #clear the current file
+    #Gather information from dspec. For example, the interval of time it needs to run....
+    #read the folders
+    #Create header for each model based on folders
+    #Call a function to format the timiing information to a cronjob format
+    #Format the string that makes the directory
+    #Format the string that will run the model 
+    #MAKE SURE THERE IS AN EXTRA SPACE AT END OF FILE
+
+#main function
