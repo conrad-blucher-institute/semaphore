d815d27d37c43f65805eff683f9dc450c2def89d
Savannah Stephenson <stephensonsavannah@ymail.com>
2024-05-22 00:34:59 -0500

    adding some bones to the flow of the program for consideration

--- tools/init_cron.py
+++ tools/init_cron.py
@@ -14,6 +14,27 @@
 # Imports
 import subprocess, os
 
+def create_model_list():
+    pass
+
+def format_timing(model_info)-> str:
+    """ A function to format the timing information in a model's despc into 
+        the cron job format: 
+        
+        5 * * * * 
+        = model runs every hour and five minutes
+    """
+    pass
+
+def format_logging_string():
+    """ A function to format the logging string according to the model's information:
+        
+        mkdir -p ./logs/$(date "+\%Y")/$(date "+\%m")/Inundation/January &&
+        
+        The && sign is at the end of the log file folder creation so that the 
+        next command in the job is still run. 
+    """
+    pass
 
 def main():
     """ The main function of init_cron.py reads through the despc folder to 
@@ -21,17 +42,36 @@ def main():
     """
     print('Initializing Cron File...')
 
-    
+
     #Gather information from dspec. For example, the interval of time it needs to run....
     #read the folders
-    #Create header for each model based on folders
-    #Call a function to format the timiing information to a cronjob format
-    #Format the string that makes the directory
-    #Format the string that will run the model 
-    #MAKE SURE THERE IS AN EXTRA SPACE AT END OF FILE
+    #Create header for each model based on folders????????
+    models_list = create_model_list()
 
-    # Clear out the cron file SAFELY
+    for model in models_list:
+        #Call a function to format the timing information to a cronjob format
+        formatted_timing =+ format_timing(model)
+    
+        #Format the string that makes the directory
+        formatted_logging_string = format_logging_string()
+
+        #Format the string that will run the model 
+        model_file_path = model.modelFileName
+
+        #Put formatted pieces together
+        cronjob = f'{formatted_timing} mkdir -p ./logs/$(date "+\%Y")/$(date "+\%m")/{formatted_logging_string} && docker exec semaphore-core python3 src/semaphoreRunner.py -d ./{model_file_path}.json >> ./logs/$(date "+\%Y")/$(date "+\%m")/{formatted_logging_string}.log 2>> ./logs/$(date "+\%Y")/$(date "+\%m")/{formatted_logging_string}_CRON.log'
+
+        # Write formatted cron job to semaphore.cron
+        with open("semaphore.cron", "a") as file:
+            file.write(cronjob+"\n")
+
+    #MAKE SURE THERE IS AN EXTRA SPACE AT END OF FILE
+    with open("semaphore.cron", "a") as file:
+            file.write("\n")
+
+    # Clear out the old cron file SAFELY
     subprocess.run(['crontab', '-r'])
+
     # Make a new cron file using the local file we have written to
     subprocess.run(['crontab', './data/cron/semaphore.cron'])
     
