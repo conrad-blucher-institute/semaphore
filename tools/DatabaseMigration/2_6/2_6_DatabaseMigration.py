086f1eb6bb4166ed73070233fcddc090e9372ec9
Savannah Stephenson <stephensonsavannah@ymail.com>
2024-09-24 14:20:16 -0500

    trying to edit yml dynamically

--- tools/DatabaseMigration/2_6/2_6_DatabaseMigration.py
+++ tools/DatabaseMigration/2_6/2_6_DatabaseMigration.py
@@ -17,6 +17,7 @@ from DatabaseMigration.IDatabaseMigration import IDatabaseMigration
 from sqlalchemy import Engine
 from sqlalchemy.sql import text
 from dotenv import load_dotenv
+import yaml
 import os
 
 # load variabesl from .env file
@@ -34,15 +35,20 @@ class Migrator(IDatabaseMigration):
         # Setting engine
         self.__engine = databaseEngine
 
+        # Check if all environment variables are set
+        if not self.__check_env_variables():
+            return False
+
         # Read user credentials from environment variables
-        api_user = os.getenv('API_USER')
-        api_password = os.getenv('API_PASSWORD')
+        api_user = os.getenv('API_POSTGRES_USER')
+        api_password = os.getenv('API_POSTGRES_PASSWORD')
         
-        core_user = os.getenv('CORE_USER')
-        core_password = os.getenv('CORE_PASSWORD')
+        core_user = os.getenv('CORE_POSTGRES_USER')
+        core_password = os.getenv('CORE_POSTGRES_PASSWORD')
         
-        general_user = os.getenv('GENERAL_USER')
-        general_password = os.getenv('GENERAL_PASSWORD')
+        general_user = os.getenv('GENERAL_POSTGRES_USER')
+        general_password = os.getenv('GENERAL_POSTGRES_PASSWORD')
+        
 
         # Create and Set Users
         self.__create_api_user(api_user, api_password)
@@ -51,6 +57,9 @@ class Migrator(IDatabaseMigration):
 
         # Check if users were added successfully
         if self.__check_user_exists(api_user) and self.__check_user_exists(core_user) and self.__check_user_exists(general_user):
+            # Update the yml file so that the different accounts are actually being used
+            self.__update_yml_file()
+            # Return True
             return True
         else:
             return False 
@@ -65,10 +74,14 @@ class Migrator(IDatabaseMigration):
         # Setting engine
         self.__engine = databaseEngine
 
+        # Check if all environment variables are set
+        if not self.__check_env_variables():
+            return False
+
         # Read users to Rollback
-        api_user = os.getenv('API_USER')
-        core_user = os.getenv('CORE_USER')
-        general_user = os.getenv('GENERAL_USER')
+        api_user = os.getenv('API_POSTGRES_USER')
+        core_user = os.getenv('CORE_POSTGRES_USER')
+        general_user = os.getenv('GENERAL_POSTGRES_USER')
 
         # Remove Users
         self.__remove_user(api_user)
@@ -77,9 +90,43 @@ class Migrator(IDatabaseMigration):
 
         # Check if users were removed successfully
         if not (self.__check_user_exists(api_user) or self.__check_user_exists(core_user) or self.__check_user_exists(general_user)):
+            # Update the yml file so that the one account we had before is being used
+            self.__rollback_yml_file()
+            # Return True
             return True
         else:
             return False
+        
+    
+    def __check_env_variables(self)-> bool:
+        """Checks if all required environment variables are set."""
+        
+        # List of required environment variables
+        required_vars = [
+            'API_POSTGRES_USER',
+            'API_POSTGRES_PASSWORD',
+            'CORE_POSTGRES_USER',
+            'CORE_POSTGRES_PASSWORD',
+            'GENERAL_POSTGRES_USER',
+            'GENERAL_POSTGRES_PASSWORD',
+            'POSTGRES_DB'
+        ]
+        
+        missing_vars = []
+        
+        # Check each environment variable
+        for var in required_vars:
+            if os.getenv(var) is None:
+                missing_vars.append(var)
+        
+        # If there are missing variables, raise an error or print a message
+        if missing_vars:
+            print(f"Error: The following environment variables are missing: {', '.join(missing_vars)}")
+            return False
+        
+        # If all variables are set, return True
+        print("All required environment variables are set.")
+        return True
 
 
     def __create_general_user(self, user: str, password: str):
@@ -88,7 +135,7 @@ class Migrator(IDatabaseMigration):
             :param: user - str - The username for the user account.
             :param: password - str - The password for the user account.
         """
-        with self.__engine.connect() as conn:
+        with self.__engine.begin() as conn:
             # Create a new user
             conn.execute(text(f"CREATE USER {user} WITH PASSWORD '{password}';"))
             # Grant connect on the database to the new user
@@ -100,8 +147,6 @@ class Migrator(IDatabaseMigration):
             # Ensure that any future tables and sequences created in the 'public' schema also grant appropriate permissions
             conn.execute(text(f"ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO {user};"))
             conn.execute(text(f"ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON SEQUENCES TO {user};"))
-            # Commit Changes
-            conn.commit()
 
 
     def __create_api_user(self, user: str, password: str):
@@ -110,7 +155,7 @@ class Migrator(IDatabaseMigration):
             :param: user - str - The username for the user account.
             :param: password - str - The password for the user account.
         """
-        with self.__engine.connect() as conn:
+        with self.__engine.begin() as conn:
             # Create a new user
             conn.execute(text(f"CREATE USER {user} WITH PASSWORD '{password}';"))
             # Grant connect on the database to the new user
@@ -122,8 +167,6 @@ class Migrator(IDatabaseMigration):
             # Ensure that any future tables and sequences created in the 'public' schema also grant appropriate permissions
             conn.execute(text(f"ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE ON TABLES TO {user};"))
             conn.execute(text(f"ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, UPDATE ON SEQUENCES TO {user};"))
-            # commit Changes
-            conn.commit()
 
 
     def __create_core_user(self, user: str, password: str):
@@ -132,7 +175,7 @@ class Migrator(IDatabaseMigration):
             :param: user - str - The username for the user account.
             :param: password - str - The password for the user account.
         """
-        with self.__engine.connect() as conn:
+        with self.__engine.begin() as conn:
             # Create a new user with superuser privileges
             conn.execute(text(f"CREATE ROLE {user} WITH LOGIN SUPERUSER PASSWORD '{password}';"))
             # Grant connect on the database to the new user
@@ -141,7 +184,6 @@ class Migrator(IDatabaseMigration):
             conn.execute(text(f"GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO {user};"))
             # Grant all permissions on all sequences in public schema to the new user
             conn.execute(text(f"GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO {user};"))
-            conn.commit()
 
 
     def __check_user_exists(self, user: str) -> bool:
@@ -155,6 +197,40 @@ class Migrator(IDatabaseMigration):
             return result.fetchone() is not None
 
 
+    def __update_yml_file(self):
+        """ Updates the .yml file with new user details.
+        """
+        # Getting yml path
+        yml_path = os.path.join(os.path.dirname(__file__), 'docker-compose.yml')
+
+        # Opening the yml file
+        with open(yml_path, 'r') as file:
+            config = yaml.safe_load(file)
+
+        # Modify the container details to use correct connection strings
+        if 'services' in config and 'core' in config['services']:
+            service = config['services']['core']
+            if 'environment' in service:
+                # Update the DB_LOCATION_STRING
+                for i, env_var in enumerate(service['environment']):
+                    if env_var.startswith('DB_LOCATION_STRING'):
+                        service['environment'][i] = 'DB_LOCATION_STRING=${CORE_DB_LOCATION_STRING}'
+                        break
+        
+        if 'services' in config and 'api' in config['services']:
+            service = config['services']['api']
+            if 'environment' in service:
+                # Update the DB_LOCATION_STRING
+                for i, env_var in enumerate(service['environment']):
+                    if env_var.startswith('DB_LOCATION_STRING'):
+                        service['environment'][i] = 'DB_LOCATION_STRING=${API_DB_LOCATION_STRING}'
+                        break
+
+        # Write back the updated config
+        with open(yml_path, 'w') as file:
+            yaml.dump(config, file)
+
+
     def __remove_user(self, user: str):
         """ Removes a user from the database if it exists.
         
@@ -163,3 +239,37 @@ class Migrator(IDatabaseMigration):
         with self.__engine.connect() as conn:
             conn.execute(text(f"DROP ROLE IF EXISTS {user};"))
             conn.commit()
+            
+
+    def __rollback_yml_file(self):
+        """ Updates the .yml file with new user details.
+        """
+        # Getting yml path
+        yml_path = os.path.join(os.path.dirname(__file__), 'docker-compose.yml')
+        
+        # Opening the yml file
+        with open(yml_path, 'r') as file:
+            config = yaml.safe_load(file)
+
+        # Modify the container details to use correct connection strings
+        if 'services' in config and 'core' in config['services']:
+            service = config['services']['core']
+            if 'environment' in service:
+                # Update the DB_LOCATION_STRING
+                for i, env_var in enumerate(service['environment']):
+                    if env_var.startswith('DB_LOCATION_STRING'):
+                        service['environment'][i] = 'DB_LOCATION_STRING=${DB_LOCATION_STRING}'
+                        break
+        
+        if 'services' in config and 'api' in config['services']:
+            service = config['services']['api']
+            if 'environment' in service:
+                # Update the DB_LOCATION_STRING
+                for i, env_var in enumerate(service['environment']):
+                    if env_var.startswith('DB_LOCATION_STRING'):
+                        service['environment'][i] = 'DB_LOCATION_STRING=${DB_LOCATION_STRING}'
+                        break
+
+        # Write back the updated config
+        with open(yml_path, 'w') as file:
+            yaml.dump(config, file, default_flow_style=False)
