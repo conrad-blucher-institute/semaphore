3842f93b230cd876d4b80e1dfe9386d1b53f23de
matdenkas <mateokastl@gmail.com>
2025-01-30 11:35:35 -0600

    Updating semaphore Runner to have dspec paths be interpreted raw instead of assuming they are somewhere

--- src/semaphoreRunner.py
+++ src/semaphoreRunner.py
@@ -197,12 +197,10 @@ def clean_and_check_dspec(dspec_path: str) -> str:
     if not dspec_path.endswith('.json'): 
         dspec_path = dspec_path + '.json'
 
-    dspecFilePath = path.join(construct_true_path(getenv('DSPEC_FOLDER_PATH')), dspec_path)
-
-    if not path.exists(dspecFilePath):
-        log(f'{dspecFilePath} not found!')
+    if not path.exists(dspec_path):
+        log(f'{dspec_path} not found!')
         raise FileNotFoundError
-    return dspecFilePath
+    return dspec_path
 
 
 
