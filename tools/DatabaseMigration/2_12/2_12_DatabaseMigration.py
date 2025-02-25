9747ccb34bbb35b754cd97dbba4ac3d48e4d79ae
jeremiahsosa77 <jeremiahsosa27@gmail.com>
2025-02-25 17:25:04 -0600

    removed artifact from database

--- tools/DatabaseMigration/2_12/2_12_DatabaseMigration.py
+++ tools/DatabaseMigration/2_12/2_12_DatabaseMigration.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 #2_10_DatabaseMigration.py
 #----------------------------------
-# Created By: Anointiyae Beasley
-# Created Date: 1/22/2024
+# Created By: Jeremiah Sosa
+# Created Date: 2/25/2025
 # Version 1.0
 #----------------------------------
 """This is a database migration script that will initialize version
     2.12 of the database. The change from version 2.11 to 2.12 will 
-    remove the ...
+    remove the artifact in the dataSourceLocationCode column and replaces it 
+    with a 0.
  """ 
 #----------------------------------
 # 
 #
 #Imports
 from DatabaseMigration.IDatabaseMigration import IDatabaseMigration
+from DatabaseMigration.databaseMigrationUtility import DatabaseDeletionHelper
 from sqlalchemy import MetaData, Engine, update
 from datetime import datetime
 
@@ -26,27 +28,19 @@ class Migrator(IDatabaseMigration):
            :return: bool indicating successful update
         """
 
-        # Reflect the tables from the database that we want to add data too
-        metadata = MetaData()
-        metadata.reflect(bind=databaseEngine)
-        
-        ref_dataLocation = metadata.tables['ref_dataLocation']
-        
-        #starting a transaction
-        with databaseEngine.begin() as connection:
+        deletion_helper = DatabaseDeletionHelper(databaseEngine)
 
-            connection.execute(
-                update(ref_dataLocation)
-                .where(ref_dataLocation.c.code == 'lagunamadre')
-                .values(latitude=26.75, longitude=-97.4167)
-            )
-        
+        deletion_helper.delete_mapping_row(
+            dataLocationCode = 'lagunamadre',
+            dataSourceCode = 'LIGHTHOUSE'
+        )
+
         return True
         
 
     def rollback(self, databaseEngine: Engine) -> bool:
         """This function rolls the database back to version 2.11 which involves
-           removing the changes associated with version 2.12 (- - - -)
+           removing the changes associated with version 2.12.
            :param databaseEngine: Engine - the engine of the database we are connecting to (semaphore)
            :return: bool indicating successful update
         """
@@ -55,15 +49,18 @@ class Migrator(IDatabaseMigration):
         metadata = MetaData()
         metadata.reflect(bind=databaseEngine)
         
-        ref_dataLocation = metadata.tables['ref_dataLocation']
+        dataLocation_dataSource_mapping = metadata.tables['dataLocation_dataSource_mapping']
         
         #starting a transaction
         with databaseEngine.begin() as connection:
 
             connection.execute(
-                update(ref_dataLocation)
-                .where(ref_dataLocation.c.code == 'lagunamadre')
-                .values(latitude=0, longitude=0)
+                dataLocation_dataSource_mapping.insert().values(
+                    dataLocationCode = 'lagunamadre',
+                    dataSourceCode = 'LIGHTHOUSE',
+                    dataSourceLocationCode = '48646513645',
+                    priorityOrder = 0
+                )
             )
 
         return True
