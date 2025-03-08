ae393cf09c995053a394b17e99cc65bc0a0af338
Savannah Stephenson <stephensonsavannah@ymail.com>
2025-03-07 20:38:17 -0600

    file set up

---
+++ src/tests/IntegrationTests/test_WEATHERCOMPANY.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# test_WEATHERCOMPANY.py
+#-------------------------------
+# Created By : Savannah Stephenson
+# Version : 1.0
+#-------------------------------
+""" This file tests the series provider class
+""" 
+#-------------------------------
+# 
+#
+#Imports
+import sys
+sys.path.append('/app/src')
+from dotenv import load_dotenv
+import pytest
+
