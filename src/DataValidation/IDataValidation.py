bf915b09ecba5646ac767e7a24b757b40405f190
matdenkas <mateokastl@gmail.com>
2025-09-16 16:57:15 -0500

    Adding DataValidation as its own decoupled module

---
+++ src/DataValidation/IDataValidation.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+#IDataValidation.py
+#----------------------------------
+# Created By: Matthew Kastl
+# Created Date: 9/16/2025
+# version 1.0
+#----------------------------------
+"""This module serves as both an interface for enhancing ingested data through post-processing 
+and a factory for generating instances of the interface.
+ """ 
+#----------------------------------
+# 
+#
+#Imports
+from abc import ABC, abstractmethod
+from importlib import import_module
+from DataClasses import Series
+
+class IDataValidation(ABC):
+    """ An interface that defines a method for validating a Series object.
+        Classes that implement this interface should provide their own implementation of the validate method.
+    """
+    @abstractmethod
+    def validate(self, series: Series) -> bool:
+        raise NotImplementedError
+    
+def data_validation_factory(dataValidationRequest: str) -> IDataValidation :
+    """ A factory method that generates an instance of a class that implements the IDataValidation interface.
+        The class is dynamically imported based on the dataValidationRequest parameter.
+        :param dataValidationRequest: str - The name of the class to instantiate.
+        :return: IDataValidation - An instance of a class that implements the IDataValidation interface.
+    """
+    try:
+        MODULE_NAME = 'DataValidationClasses'
+        return getattr(import_module(f'.{MODULE_NAME}.{dataValidationRequest}', 'DataValidation'), dataValidationRequest)()
+    except (ModuleNotFoundError, AttributeError) as e:
+        raise ImportError(f'Error importing post-processing class {dataValidationRequest}: {e}')
