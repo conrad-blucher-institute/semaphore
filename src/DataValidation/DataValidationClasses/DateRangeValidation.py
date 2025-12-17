1aa72f535a324699b545c0bf1be8bcea1529526f
CJQuintero61 <cjquintero61@gmail.com>
2025-12-16 20:37:06 -0600

    changed log statements to print statements

--- src/DataValidation/DataValidationClasses/DateRangeValidation.py
+++ src/DataValidation/DataValidationClasses/DateRangeValidation.py
@@ -31,7 +31,7 @@ class DateRangeValidation(IDataValidation):
         """
 
         if series.dataFrame is None or len(series.dataFrame) <= 0:
-            log('DateRangeValidation: No data in series to validate.')
+            print('DateRangeValidation: No data in series to validate.')
             return False # No data to validate
     
         df_to_validate = series.dataFrame.copy()
@@ -48,9 +48,9 @@ class DateRangeValidation(IDataValidation):
         missing_value_count = df_to_validate['dataValue'].isnull().sum()
         
         if missing_value_count > 0:
-            log(f'DateRangeValidation: Series {series} is missing {missing_value_count} values.')
+            print(f'DateRangeValidation: Series {series} is missing {missing_value_count} values.')
             for missing_time in df_to_validate[df_to_validate['dataValue'].isnull()].index:
-                log(f'\tMissing time: {missing_time}')
+                print(f'\tMissing time: {missing_time}')
             return False
         
         # only unit tests will skip this check unless they set a reference time
@@ -62,8 +62,8 @@ class DateRangeValidation(IDataValidation):
 
             # validate that the data isn't stale 
             if time_difference > series.timeDescription.stalenessOffset:
-                log(f'DateRangeValidation: Series {series} is stale.\n')
-                log(f'Time difference: {time_difference}. Staleness offset: {series.timeDescription.stalenessOffset}\n')
+                print(f'DateRangeValidation: Series {series} is stale.\n')
+                print(f'Time difference: {time_difference}. Staleness offset: {series.timeDescription.stalenessOffset}\n')
                 return False
         
         return True
