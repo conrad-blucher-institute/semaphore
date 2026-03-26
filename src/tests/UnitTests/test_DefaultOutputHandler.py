1ff660ca388ce54eb74a5f55889cc2bdc95118e2
Christian Quintero <cjquintero61@gmail.com>
2026-03-26 14:30:25 -0500

    updated logic in output handler and fixed tests

--- src/tests/UnitTests/test_DefaultOutputHandler.py
+++ src/tests/UnitTests/test_DefaultOutputHandler.py
@@ -27,11 +27,61 @@ from pandas import DataFrame
 
 TEST_REF_TIME = datetime(2021, 1, 1, 0, 0)
 
-SCALAR_2D_PREDICTION = np.array([[0.7075105]], dtype=np.float32)
-MULTI_2D_PREDICTION  = np.array([[i] for i in range(100)], dtype=np.float32)
+# shape of (1,1,1)
+SCALAR_PREDICTION = np.array(
+    [
+        [
+            [0.7075105]
+        ]
+    ],
+    dtype=np.float32
+)
+# shape of (1,100,1)
+ENSEMBLE_PREDICTION  = np.array(
+    [
+        [
+            [1],  [2],  [3],  [4],  [5],  [6],  [7],  [8],  [9],  [10],
+            [11], [12], [13], [14], [15], [16], [17], [18], [19], [20],
+            [21], [22], [23], [24], [25], [26], [27], [28], [29], [30],
+            [31], [32], [33], [34], [35], [36], [37], [38], [39], [40],
+            [41], [42], [43], [44], [45], [46], [47], [48], [49], [50],
+            [51], [52], [53], [54], [55], [56], [57], [58], [59], [60],
+            [61], [62], [63], [64], [65], [66], [67], [68], [69], [70],
+            [71], [72], [73], [74], [75], [76], [77], [78], [79], [80],
+            [81], [82], [83], [84], [85], [86], [87], [88], [89], [90],
+            [91], [92], [93], [94], [95], [96], [97], [98], [99], [100]
+        ] 
+    ],
+    dtype=np.float32
+)
+# shape of (3, 5, 8)
+MULTI_MEMBER_PREDICTION = np.array(
+    [
+        [
+            [1.0,   2.0,   3.0,   4.0,   5.0,   6.0,   7.0,   8.0],
+            [9.0,   10.0,  11.0,  12.0,  13.0,  14.0,  15.0,  16.0],
+            [17.0,  18.0,  19.0,  20.0,  21.0,  22.0,  23.0,  24.0],
+            [25.0,  26.0,  27.0,  28.0,  29.0,  30.0,  31.0,  32.0],
+            [33.0,  34.0,  35.0,  36.0,  37.0,  38.0,  39.0,  40.0]
+        ],
+        [
+            [41.0,  42.0,  43.0,  44.0,  45.0,  46.0,  47.0,  48.0],
+            [49.0,  50.0,  51.0,  52.0,  53.0,  54.0,  55.0,  56.0],
+            [57.0,  58.0,  59.0,  60.0,  61.0,  62.0,  63.0,  64.0],
+            [65.0,  66.0,  67.0,  68.0,  69.0,  70.0,  71.0,  72.0],
+            [73.0,  74.0,  75.0,  76.0,  77.0,  78.0,  79.0,  80.0]
+        ],
+        [
+            [81.0,  82.0,  83.0,  84.0,  85.0,  86.0,  87.0,  88.0],
+            [89.0,  90.0,  91.0,  92.0,  93.0,  94.0,  95.0,  96.0],
+            [97.0,  98.0,  99.0,  100.0, 101.0, 102.0, 103.0, 104.0],
+            [105.0, 106.0, 107.0, 108.0, 109.0, 110.0, 111.0, 112.0],
+            [113.0, 114.0, 115.0, 116.0, 117.0, 118.0, 119.0, 120.0]
+        ]
+    ]
+)
 
-
-def make_dspec(inputVectorCount: int) -> Dspec:
+def make_dspec(memberCount: int = None, inputVectorCount: int = None, outputCount: int = None) -> Dspec:
     d = Dspec()
 
     oi = OutputInfo()
@@ -40,9 +90,9 @@ def make_dspec(inputVectorCount: int) -> Dspec:
     oi.unit = "testUnit"
 
     eos = ExpectedOutputShape()
-    eos.memberCount = 1
+    eos.memberCount = memberCount
     eos.inputVectorCount = inputVectorCount
-    eos.outputsPerVector = 1
+    eos.outputsPerVector = outputCount
 
     oi.expectedOutputShape = eos
     d.outputInfo = oi
@@ -52,8 +102,9 @@ def make_dspec(inputVectorCount: int) -> Dspec:
 @pytest.mark.parametrize(
     "predictions, dspec, expected_shape",
     [
-        (SCALAR_2D_PREDICTION, make_dspec(1),   (1, 1, 1)),
-        (MULTI_2D_PREDICTION,  make_dspec(100), (1, 100, 1)),
+        (SCALAR_PREDICTION, make_dspec(1,1,1),   (1, 1, 1)),
+        (ENSEMBLE_PREDICTION,  make_dspec(1,100,1), (1, 100, 1)),
+        (MULTI_MEMBER_PREDICTION, make_dspec(3,5,8), (3, 5, 8)),
     ],
 )
 def test_post_process_prediction(predictions: np.ndarray, dspec: Dspec, expected_shape: tuple):
@@ -70,9 +121,9 @@ def test_post_process_prediction(predictions: np.ndarray, dspec: Dspec, expected
     assert result.loc[0, "leadTime"] == timedelta(seconds=3600)
 
     # --- tensor checks ---
-    actual_tensor = np.array(result.loc[0, "dataValue"])
-    assert actual_tensor.shape == expected_shape
+    result_tensor = np.array(result.loc[0, "dataValue"])
+    assert result_tensor.ndim == 3
+    assert result_tensor.shape == expected_shape
 
-    # Compare numeric values
-    expected_tensor = predictions.reshape(1, predictions.shape[0], predictions.shape[1])
-    np.testing.assert_allclose(actual_tensor, expected_tensor, rtol=0, atol=0)
+    # ensure values are not changed in the output handler
+    np.testing.assert_allclose(result_tensor, predictions, rtol=0, atol=0)
