8067a6dbe70a3870d1835ae9894c3cc13911c960
Christian Quintero <cjquintero61@gmail.com>
2026-08-31 15:08:18 -0500

    removed unused imports

--- tools/DatabaseMigration/3_8/3_8_DatabaseMigration.py
+++ tools/DatabaseMigration/3_8/3_8_DatabaseMigration.py
@@ -15,7 +15,7 @@ for the operation of the ESB cold stunning model.
 #
 # Imports
 from DatabaseMigration.IDatabaseMigration import IDatabaseMigration
-from sqlalchemy import Engine, MetaData, Table
+from sqlalchemy import Engine
 from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.sql import text
 import csv
