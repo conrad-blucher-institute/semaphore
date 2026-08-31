6da92a05fef7e04b0f40b064ed9d200cda283e64
Christian Quintero <cjquintero61@gmail.com>
2026-08-31 15:03:17 -0500

    added db migration v3.8

--- tools/DatabaseMigration/3_8/3_8_DatabaseMigration.py
+++ tools/DatabaseMigration/3_8/3_8_DatabaseMigration.py
@@ -1,107 +1,127 @@
 # -*- coding: utf-8 -*-
 # 3_8_DatabaseMigration.py
 # ----------------------------------
-# Created By: Anointiyae Beasley
-# Created Date: 08/19/2026
+# Created By: CJ Quintero
+# Created Date: 08/31/2026
 # Version 1.0
 # ----------------------------------
 """
-This db migration script adds entries for Seadrift and Aransas Wildlife Refuge
-to the ref_dataLocation table and dataLocation_dataSource_mapping
-table for the operation of cold stunning models for Espiritu Santo Bay.
+This db migration script adds Seadrift, Aransas Wildlife Refuge, and ESB to the ref_dataLocation table
+and adds Port Lavaca, Seadrift, and Aransas Wildlife Refuge to the dataLocation_dataSource_mapping table
+for the operation of the ESB cold stunning model.
 """ 
 # ----------------------------------
 # 
 #
 # Imports
 from DatabaseMigration.IDatabaseMigration import IDatabaseMigration
-from DatabaseMigration.databaseMigrationUtility import KeywordType, DatabaseDeletionHelper
-from sqlalchemy import Engine, MetaData, Table, delete, select
+from sqlalchemy import Engine, MetaData, Table
 from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.sql import text
 import csv
 
 # Constants
-CSV_FILE_PATHS = './tools/DatabaseMigration/3_8/init_data'
+MAPPING_CSV = './tools/DatabaseMigration/3_8/init_data/dataMapping.csv'
+LOCATION_CSV = './tools/DatabaseMigration/3_8/init_data/dataLocation.csv'
 
 
 class Migrator(IDatabaseMigration):
 
     def update(self, databaseEngine: Engine) -> bool:
         """
-        This function updates the database to version 3.8 which adds row for
-        Seadrift and Aransas Wildlife Refuge to the ref_dataLocation table
-        and the dataLocation_dataSource_mapping table. 
+        This function updates the database to version 3.8 which adds rows to the
+        dataLocation_dataSource_mapping table and the ref_dataLocation table.
+        The rows added are read from the dataLocation.csv and dataMapping.csv files.
 
         :param databaseEngine: Engine - the engine of the database we are connecting to (semaphore)
 
         :return: bool indicating successful update
+
+        NOTE: Both tables use strings as the data type for all values except for priorityOrder which
+        is an integer.
         """
-        # Setting engine
-        self.__engine = databaseEngine
+        # read the rows 
+        location_rows, mapping_rows = self._read_rows()
 
-        # Reflect the tables from the database that we want to add data too
-        metadata = MetaData()
-        metadata.reflect(bind=databaseEngine)
-        
-        ref_dataLocation = metadata.tables['ref_dataLocation']
-        ref_dataLocation_dataSource_mapping = metadata.tables['dataLocation_dataSource_mapping']
+        with databaseEngine.connect() as conn:
+            # conn.begin() will commit a single transaction at the end of the with block
+            with conn.begin():
 
-        # Insert the data based off of csv files
-        self.insert_ref_data(self.readInitCSV('dataLocation.csv'), ref_dataLocation)
-        self.insert_ref_data(self.readInitCSV('dataMapping.csv'), ref_dataLocation_dataSource_mapping)
+                # insert rows into the location table
+                for row in location_rows:
+                    stmt = text("""
+                    INSERT INTO "ref_dataLocation" (
+                        "code",
+                        "displayName",
+                        "notes",
+                        "latitude",
+                        "longitude"
+                    ) 
+                    VALUES (
+                        :code,
+                        :displayName,
+                        :notes,
+                        :latitude,
+                        :longitude
+                    )
+                    """)
+                    bind_params = {
+                        "code": row["code"],
+                        "displayName": row["displayName"],
+                        "notes": row["notes"],
+                        "latitude": row["latitude"],
+                        "longitude": row["longitude"]
+                    }
+                    stmt = stmt.bindparams(**bind_params)
+                    conn.execute(stmt)
+
+                # insert rows into the mapping table
+                for row in mapping_rows:
+                    stmt = text("""
+                    INSERT INTO "dataLocation_dataSource_mapping" (
+                        "dataLocationCode",
+                        "dataSourceCode",
+                        "dataSourceLocationCode",
+                        "priorityOrder"
+                    ) 
+                    VALUES (
+                        :dataLocationCode,
+                        :dataSourceCode,
+                        :dataSourceLocationCode,
+                        :priorityOrder
+                    )
+                    """)
+                    bind_params = {
+                        "dataLocationCode": row["dataLocationCode"],
+                        "dataSourceCode": row["dataSourceCode"],
+                        "dataSourceLocationCode": row["dataSourceLocationCode"],
+                        "priorityOrder": int(row["priorityOrder"])
+                    }
+                    stmt = stmt.bindparams(**bind_params)
+                    conn.execute(stmt)
         
         return True
     
-    def readInitCSV(self, csvFileName: str) -> list:
+    def _read_rows(self) -> tuple[list[dict], list[dict]]:
         """
-        This function reads in a CSV file with the data needed for the initialization 
-        of the database
+        This function reads rows from the dataLocation.csv and dataMapping.csv files and returns them
+        as a list of dictionaries
 
-        :param csvFileName: str - CSV file name
-            
-        :return: list of dictionaries
+        :return tuple[list[dict], list[dict]] - a tuple of 2 lists of dictionaries representing each row in the csv files.
+            The first element is the list of dictionaries from the dataLocation csv file
+            and the second element is the list of dictionaries from the dataMapping csv file.
         """
-        csvFilePath = f'{CSV_FILE_PATHS}/{csvFileName}'
-        dictionaryList = []
-        with open(csvFilePath, mode = 'r') as infile:
-            csvDict = csv.DictReader(infile)
-            for dictionary in csvDict:
-                dictionaryList.append(dictionary)
 
-        return dictionaryList
-    
-    def insert_ref_data(self, rows: list[dict], table: Table) -> list[tuple]:
-        """
-        This method inserts rows into a table in the database.
+        # read rows that were added from the data location csv file
+        with open(f'{LOCATION_CSV}', mode='r') as file:
+            location_rows = [row for row in csv.DictReader(file)]
 
-        :param rows: A list of dictionaries to insert into a table.
-            for the ref_dataLocation table, the dictionaries have the format:
-                {
-                    "code": None,
-                    "displayName": None,
-                    "notes": None,
-                    "latitude": None,
-                    "longitude": None
-                }
-            
-            for the dataLocation_dataSource_mapping table, the dictionaries have the format:
-                {
-                    "dataLocationCode": None,
-                    "dataSourceCode": None,
-                    "dataSourceLocationCode": None,
-                    "priorityOrder": None
-                }
-        
-        :return Series - SQLALCHEMY tupleish rows
-        """
-        with self.__engine.connect() as conn:
-            cursor = conn.execute(insert(table)
-                                  .returning(table)
-                                  .values(rows)
-                                  )
-            result = cursor.fetchall()
-            conn.commit()
-            return result
+        # read rows that were added from the data mapping csv file
+        with open(f'{MAPPING_CSV}', mode='r') as file:
+            mapping_rows = [row for row in csv.DictReader(file)]
+
+        return location_rows, mapping_rows
+
 
     def rollback(self, databaseEngine: Engine) -> bool:
         """
@@ -113,14 +133,41 @@ class Migrator(IDatabaseMigration):
 
         :return: bool indicating successful update
         """
-        fileNames = ['dataLocation.csv']
-        fileTypes = [KeywordType.DATA_LOCATION]
 
-        # Using the utility helper class to delete any data dependent on the rows added in the 3.8 Migration
-        helper = DatabaseDeletionHelper(databaseEngine)
+        location_rows, mapping_rows = self._read_rows()
 
-        for file, type in zip(fileNames, fileTypes):
-            for rowDict in self.readInitCSV(file):
-                helper.deep_delete_keyword(rowDict["code"], type)
+        with databaseEngine.connect() as conn:
+            # conn.begin() will commit a single transaction at the end of the with block
+            with conn.begin():
+
+                # delete rows from the mapping table first
+                for row in mapping_rows:
+                    stmt = text("""
+                        DELETE FROM "dataLocation_dataSource_mapping"
+                        WHERE "dataLocationCode" = :code
+                        AND "dataSourceCode" = :source
+                    """)
+
+                    bind_params = {
+                        "code": row["dataLocationCode"],
+                        "source": row["dataSourceCode"]
+                    }
+
+                    stmt = stmt.bindparams(**bind_params)
+                    conn.execute(stmt)
+
+                # then delete rows from the location table
+                for row in location_rows:
+                    stmt = text("""
+                        DELETE FROM "ref_dataLocation"
+                        WHERE "code" = :code
+                    """)
+
+                    bind_params = {
+                        "code": row["code"]
+                    }
+
+                    stmt = stmt.bindparams(**bind_params)
+                    conn.execute(stmt)
 
         return True
