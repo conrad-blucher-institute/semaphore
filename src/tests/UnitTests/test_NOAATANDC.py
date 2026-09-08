b8e33f8cd47a596959f10e4f85963a11b80e10ab
Florence Tissot <ftissot@fastmail.us>
2026-09-08 16:26:43 -0500

    fixed prior issue with a unit test

--- src/tests/UnitTests/test_NOAATANDC.py
+++ src/tests/UnitTests/test_NOAATANDC.py
@@ -237,7 +237,7 @@ class TestNOAATANDCUnit:
             
             assert result is not None
             assert isinstance(result, Series)
-            assert result.description.dataDatum == 'NA'
+            assert result.description.dataDatum == None
             assert all(result.dataFrame['dataUnit'] == 'meter')
             assert mock_fetch.call_count == 2
     
