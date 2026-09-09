9124656228849eca31048281557428b227d56846
Christian Quintero <cjquintero61@gmail.com>
2026-09-09 10:40:03 -0500

    revert esb branch to match main

--- src/DataClasses.py
+++ src/DataClasses.py
@@ -130,8 +130,6 @@ class Series():
         self.description = description
         self.timeDescription = timeDescription
         self.__dataFrame = None
-        self.__sentinelValue = None
-
 
     @property
     def dataFrame(self):
@@ -140,15 +138,6 @@ class Series():
     @dataFrame.setter
     def dataFrame(self, dataFrame: DataFrame) -> None:
         self.__dataFrame = dataFrame
-
-    @property
-    def sentinelValue(self):
-        return self.__sentinelValue
-
-    @sentinelValue.setter
-    def sentinelValue(self, sentinelValue: int | str) -> None:
-        self.__sentinelValue = sentinelValue
-
 
     def __str__(self) -> str:
         return f'\n[Series] -> description: {self.description}, timeDescription: {self.timeDescription}'
