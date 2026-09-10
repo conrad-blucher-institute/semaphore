5624daaddc928aa6471b011cc98882ca63c6694e
Florence Tissot <ftissot@fastmail.us>
2026-09-09 19:14:47 -0500

    addressed review comments

--- src/tests/UnitTests/test_dataGatherer.py
+++ src/tests/UnitTests/test_dataGatherer.py
@@ -209,7 +209,7 @@ def test_data_integrity_call(data_gatherer, mock_dspec, mock_integrity_factory):
     dspec_copy.dependentSeries[0].dataIntegrityCall = DataIntegrityDescription(
         call= 'PandasInterpolation',
         args= {
-            'limit': 3600,
+            'limit': '7200',
             'method': 'time',
             'limit_area': 'inside'
         }
